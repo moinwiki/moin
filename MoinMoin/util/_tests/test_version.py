e4f0d6ccfdf8f6bb7b73e82b8dadadb138c0e43e
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2011-02-20 21:44:35 +0100

    use PEP386 strict version numbers

--- MoinMoin/util/_tests/test_version.py
+++ MoinMoin/util/_tests/test_version.py
@@ -15,43 +15,44 @@ class TestVersion(object):
         assert Version(1, 2, 3).major == 1
         assert Version(1, 2, 3).minor == 2
         assert Version(1, 2, 3).release == 3
-        assert Version(1, 2, 3, '4.5alpha6').additional == '4.5alpha6'
+        assert Version(1, 2, 3, 'a4').additional == 'a4'
         # test Version init and Version to str conversion
         assert str(Version(1)) == "1.0.0"
         assert str(Version(1, 2)) == "1.2.0"
         assert str(Version(1, 2, 3)) == "1.2.3"
-        assert str(Version(1, 2, 3, '4.5alpha6')) == "1.2.3-4.5alpha6"
+        assert str(Version(1, 2, 3, 'a4')) == "1.2.3a4"
         assert str(Version(version='1.2.3')) == "1.2.3"
-        assert str(Version(version='1.2.3-4.5alpha6')) == "1.2.3-4.5alpha6"
+        assert str(Version(version='1.2.3a4')) == "1.2.3a4"
         # test Version comparison, trivial cases
         assert Version() == Version()
         assert Version(1) == Version(1)
         assert Version(1, 2) == Version(1, 2)
         assert Version(1, 2, 3) == Version(1, 2, 3)
-        assert Version(1, 2, 3, 'foo') == Version(1, 2, 3, 'foo')
+        assert Version(1, 2, 3, 'a4') == Version(1, 2, 3, 'a4')
         assert Version(1) != Version(2)
         assert Version(1, 2) != Version(1, 3)
         assert Version(1, 2, 3) != Version(1, 2, 4)
-        assert Version(1, 2, 3, 'foo') != Version(1, 2, 3, 'bar')
+        assert Version(1, 2, 3, 'a4') != Version(1, 2, 3, 'a5')
         assert Version(1) < Version(2)
         assert Version(1, 2) < Version(1, 3)
         assert Version(1, 2, 3) < Version(1, 2, 4)
-        assert Version(1, 2, 3, 'bar') < Version(1, 2, 3, 'foo')
+        assert Version(1, 2, 3, 'a4') < Version(1, 2, 3, 'a5')
+        assert Version(1, 2, 3, 'b4') < Version(1, 2, 3, 'b5')
+        assert Version(1, 2, 3, 'c4') < Version(1, 2, 3, 'c5')
         assert Version(2) > Version(1)
         assert Version(1, 3) > Version(1, 2)
         assert Version(1, 2, 4) > Version(1, 2, 3)
-        assert Version(1, 2, 3, 'foo') > Version(1, 2, 3, 'bar')
+        assert Version(1, 2, 3, 'b1') > Version(1, 2, 3, 'a1')
+        assert Version(1, 2, 3, 'c1') > Version(1, 2, 3, 'b1')
         # test Version comparison, more delicate cases
+        assert Version(1, 2, 3) > Version(1, 2, 3, 'c1')
         assert Version(1, 12) > Version(1, 9)
         assert Version(1, 12) > Version(1, 1, 2)
-        assert Version(1, 0, 0, '0.0a2') > Version(1, 0, 0, '0.0a1')
-        assert Version(1, 0, 0, '0.0b1') > Version(1, 0, 0, '0.0a9')
-        assert Version(1, 0, 0, '0.0b2') > Version(1, 0, 0, '0.0b1')
-        assert Version(1, 0, 0, '0.0c1') > Version(1, 0, 0, '0.0b9')
-        assert Version(1, 0, 0, '1') > Version(1, 0, 0, '0.0c9')
         # test Version playing nice with tuples
-        assert Version(1, 2, 3) == (1, 2, 3, '')
+        assert Version(1, 2, 3) < (1, 2, 4)
+        assert Version(1, 2, 3, 'c99') < (1, 2, 4)
         assert Version(1, 2, 4) > (1, 2, 3)
+        assert Version(1, 2, 4) > (1, 2, 3, 'c99')
 
 
 coverage_modules = ['MoinMoin.util.version']
