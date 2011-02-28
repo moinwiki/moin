f114068a3727cd49ba09ed12ac2dec3fd62baa70
TW <tw@waldmann-edv.de>
2011-02-28 01:38:45 +0100

    chartypes_create: do not execute the main code if the module is just imported

--- MoinMoin/util/chartypes_create.py
+++ MoinMoin/util/chartypes_create.py
@@ -7,28 +7,29 @@
 """
 
 
-uppercase = []
-lowercase = []
-digits = []
-space = []
-for code in range(1, 65535):
-    c = unichr(code)
-    str = "\\u%04x" % code
-    if c.isupper():
-        uppercase.append(str)
-    elif c.islower():
-        lowercase.append(str)
-    elif c.isdigit():
-        digits.append(str)
-    elif c.isspace():
-        space.append(str)
+def main():
+    uppercase = []
+    lowercase = []
+    digits = []
+    space = []
+    for code in range(1, 65535):
+        c = unichr(code)
+        str = "\\u%04x" % code
+        if c.isupper():
+            uppercase.append(str)
+        elif c.islower():
+            lowercase.append(str)
+        elif c.isdigit():
+            digits.append(str)
+        elif c.isspace():
+            space.append(str)
 
-chars_upper = u''.join(uppercase)
-chars_lower = u''.join(lowercase+digits)
-chars_digits = u''.join(digits)
-chars_spaces = u''.join(space)
+    chars_upper = u''.join(uppercase)
+    chars_lower = u''.join(lowercase+digits)
+    chars_digits = u''.join(digits)
+    chars_spaces = u''.join(space)
 
-print """
+    print """
 chars_upper = u"%(chars_upper)s"
 
 chars_lower = u"%(chars_lower)s"
@@ -38,5 +39,8 @@ chars_digits = u"%(chars_digits)s"
 chars_spaces = u"%(chars_spaces)s"
 
 
-""" % globals()
+""" % locals()
+
+if __name__ == '__main__':
+    main()
 
