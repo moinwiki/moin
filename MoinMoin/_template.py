402f56f33262b6ce0ed92d9f8bbdb49228a4666c
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2011-03-07 19:31:25 +0100

    better sourcecode template

--- MoinMoin/_template.py
+++ MoinMoin/_template.py
@@ -2,9 +2,16 @@
 # License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
 
 """
-MoinMoin - <short description>
+MoinMoin - sourcecode header template
 
-<what this stuff does ... - verbose enough>
+Please use this template when starting a new python source file.
+
+It is important that all sourcecode files have the same header structure,
+are correctly copyrighted and licensed and have a reasonable docstring
+explaining what the module / package does.
+
+Of course you'll have to edit/fix the Copyright line and also the docstring
+needs to be replaced with something making sense, but keep the structure.
 """
 
 
