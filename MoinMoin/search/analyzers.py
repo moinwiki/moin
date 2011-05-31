652d9ab4f6f30a28edb03f415f95a6bf73a05f71
Michael Mayorov <marchael@kb.csu.ru>
2011-05-31 23:57:40 +0000

    Whoosh schemas for items and revisions, analyzer for "item name" field and tokenizers for "acl" and "mimetype" fields of whoosh's schema. Write simple tests for acl and mimetype tokenizers.

---
+++ MoinMoin/search/analyzers.py
@@ -0,0 +1,51 @@
+# Copyright: 2011 MoinMoin:MichaelMayorov
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+MoinMoin - MoinMoin.analyzers Tokenizers and analyzers for indexing schema
+"""
+
+
+from flask import current_app as app
+from whoosh.analysis import MultiFilter, RegexTokenizer
+from whoosh.analysis import Tokenizer, Token
+
+from MoinMoin.util.mime import Type
+from MoinMoin.security import ContentACL
+
+
+class MimeTokenizer(Tokenizer):
+    def __call__(self, value):
+        assert isinstance(value, unicode), "%r is not unicode" % value
+        tk = Token()
+        tp = Type(value)
+        tk.text = tp.type
+        yield tk
+        tk.text = tp.subtype
+        yield tk
+        for key, value in tp.parameters.items():
+            tk.text = u"%s=%s" % (key, value)
+            yield tk
+
+
+class AclTokenizer(Tokenizer):
+    def __call__(self, value):
+        assert isinstance(value, list) # so you'll notice if it blows up
+        for acl_right in value:
+            assert isinstance(acl_right, unicode), "%r is not unicode" % acl_right
+
+        tk = Token()
+        acl = ContentACL(app.cfg, value)
+        for name, permissions in acl.acl:
+            for permission in permissions:
+                sign = "+" if permissions[permission] else "-"
+                tk.text = u"%s:%s%s" % (name, sign, permission)
+                yield tk
+
+
+def item_name_analyzer():
+    iwf = MultiFilter(index=IntraWordFilter(mergewords=True, mergenums=True), 
+                      query=IntraWordFilter(mergewords=False, mergenums=False)
+                     )
+    analyzer = RegexTokenizer(r"\S+") | iwf()
+    return analyzer
