a481c750031e53bfbc934f94436d896c0b68172d
Michael Mayorov <marchael@kb.csu.ru>
2011-06-19 14:18:07 +0000

    Fiexes in flask-script for rebuilding indexes. Python2.6 compatibility. Make changes in schema for better integration with Moin.

--- MoinMoin/script/maint/build_indexes.py
+++ MoinMoin/script/maint/build_indexes.py
@@ -5,10 +5,13 @@
 MoinMoin - Build indexes
 """
 
+import sys
 
 from flask import current_app as app
 from flask import g as flaskg
 from flaskext.script import Command, Option
+from whoosh.filedb.multiproc import MultiSegmentWriter
+from whoosh.index import create_in
 
 from MoinMoin.search.indexing import WhooshIndex
 
@@ -16,24 +19,90 @@ from MoinMoin.search.indexing import WhooshIndex
 class RebuildIndexes(Command):
     description = 'Build indexes'
 
-    def run(self):
-        index_object = WhooshIndex()
+    option_list = (
+        Option('--procs', '-p', required=False, dest='procs', type=int, default=None,
+            help='Number of processors the writer. Default: 1'),
+        Option('--limitmb', '-l', required=False, dest='limitmb', type=int, default=10,
+            help='Maximum memory (in megabytes) each index-writer will use for the indexing pool. Default: 10'),
+        Option('--clean', action='store_true', dest='clean',# type=bool,
+            help='Clear index files of given index-name. ATTENITON: use it only if your indexes broke, you had backup\
+                  or just nothing to lose'),
+        Option('--build', '-b', required=True, dest='build', type=str, choices=("all-revs", "latest-revs", "both"),
+            help='What type of indexes we want to build. "all-revs", "latest-revs" or "both". Default: "both"'),
+                  )
+
+    # We use 3 separated functions because want to avoid checks and increase speed
+    def run(self, procs, limitmb, build, clean):
+        def build_both(clean):
+            """Build indexes for all and latest revisions"""
+            if clean:
+                index_object.create_index(indexdir=app.cfg.index_dir,
+                                         indexname="latest_revisions_index",
+                                         schema="latest_revisions_schema"
+                                        )
+                index_object.create_index(indexdir=app.cfg.index_dir,
+                                         indexname="all_revisions_index",
+                                         schema="all_revisions_schema"
+                                        )
+            with MultiSegmentWriter(all_rev_index, procs, limitmb) as all_rev_writer:
+                with MultiSegmentWriter(latest_rev_index, procs, limitmb) as latest_rev_writer:
+                    for item in backend.iter_items_noindex():
+                        for rev_no in item.list_revisions():
+                            revision = item.get_revision(rev_no)
+                            metadata = dict([(str(key), value)
+                                             for key, value in revision.items()
+                                             if key in all_rev_field_names])
+                            metadata_orig = dict([(str(key), value)
+                                             for key, value in revision.items()])
+                            all_rev_writer.add_document(**metadata)
+                        # revision is now the latest revision of this item
+                        metadata = dict([(str(key), value)
+                                          for key, value in revision.items()
+                                          if key in latest_rev_field_names])
+                        latest_rev_writer.add_document(**metadata)
+
+        def build_all_revs(clean):
+            """Build indexes for all revisions"""
+            if clean:
+                index_object.create_index(indexdir=app.cfg.index_dir,
+                                         indexname="all_revisions_index",
+                                         schema="all_revisions_schema"
+                                        )
+            with MultiSegmentWriter(all_rev_index, procs, limitmb) as all_rev_writer:
+                for item in backend.iter_items_noindex():
+                    for rev_no in item.list_revisions():
+                        revision = item.get_revision(rev_no)
+                        metadata = dict([(str(key), value)
+                                          for key, value in revision.items()
+                                          if key in all_rev_field_names])
+                        all_rev_writer.add_document(**metadata)
+
+        def build_latest_revs(clean):
+            """Build indexes for latest revisions"""
+            if clean:
+                index_object.create_index(indexdir=app.cfg.index_dir,
+                                         indexname="latest_revisions_index",
+                                         schema="latest_revisions_schema"
+                                        )
+            with MultiSegmentWriter(latest_rev_index, procs, limitmb) as latest_rev_writer:
+                for item in backend.iter_items_noindex():
+                    rev_no = max(item.list_revisions())
+                    revision = item.get_revision(rev_no)
+                    metadata = dict([(str(key), value)
+                                      for key, value in revision.items()
+                                      if key in latest_rev_field_names])
+                    latest_rev_writer.add_document(**metadata)
+
         backend = flaskg.unprotected_storage = app.unprotected_storage
+        index_object = WhooshIndex()
         latest_rev_index = index_object.latest_revisions_index
         latest_rev_field_names = latest_rev_index.schema.names()
         all_rev_index = index_object.all_revisions_index
         all_rev_field_names = all_rev_index.schema.names()
-        with all_rev_index.writer() as all_rev_writer:
-            for item in backend.iter_items_noindex():
-                for rev_no in item.list_revisions():
-                    revision = item.get_revision(rev_no)
-                    metadata = {key: value
-                                for key, value in revision.items()
-                                if key in all_rev_field_names}
-                    all_rev_writer.add_document(**metadata)
-                # revision is now the latest revision of this item
-                metadata = {key: value
-                            for key, value in revision.items()
-                            if key in latest_rev_field_names}
-                with latest_rev_index.writer() as latest_rev_writer:
-                    latest_rev_writer.add_document(**metadata)
+
+        if build == "both":
+            build_both(clean)
+        elif build == "all-revs":
+            build_all_revs(clean)
+        elif build == "latest-revs":
+            build_latest_revs(clean)
--- MoinMoin/search/indexing.py
+++ MoinMoin/search/indexing.py
@@ -14,7 +14,7 @@ from whoosh.analysis import entoken
 from whoosh.index import open_dir, create_in, EmptyIndexError
 
 from MoinMoin.search.analyzers import *
-
+from MoinMoin.error import FatalError
 '''
 for text items, it will be duplication. For "binary" items, it will only store
 what the filter code outputs (e.g. if it is a PDF, it will only store what
@@ -26,7 +26,7 @@ pdftotext outputs as text, if it is a mp3, it will only store title/author/genre
 class WhooshIndex(object):
 
     latest_revisions_schema = Schema(wikiname=ID(stored=True),
-                                     itemname=TEXT(stored=True, analyzer=item_name_analyzer()),
+                                     name=TEXT(stored=True, analyzer=item_name_analyzer()),
                                      uuid=ID(unique=True, stored=True),
                                      rev_no=NUMERIC(stored=True),
                                      datetime=DATETIME(stored=True),
@@ -43,7 +43,7 @@ class WhooshIndex(object):
                                     )
 
     all_revisions_schema = Schema(wikiname=ID(stored=True),
-                                  itemname=TEXT(stored=True, analyzer=item_name_analyzer()),
+                                  name=TEXT(stored=True, analyzer=item_name_analyzer()),
                                   uuid=ID(stored=True),
                                   rev_no=NUMERIC(stored=True),
                                   datetime=DATETIME(stored=True),
@@ -56,16 +56,14 @@ class WhooshIndex(object):
                                   hostname=ID(stored=True),
                                  )
 
-    indexes = [('latest_revisions_index', 'latest_revisions_schema'),
-               ('all_revisions_index', 'all_revisions_schema'),
-              ]
+    indexes = {'latest_revisions_index': 'latest_revisions_schema',
+               'all_revisions_index': 'all_revisions_schema',
+              }
 
     def __init__(self, index_dir=None):
         index_dir = index_dir or app.cfg.index_dir
-        for index_name, index_schema in self.indexes:
-            index = self.open_index(index_dir, index_name, index_schema, create=True)
-            setattr(self, index_name, index)
-
+        for index_name, index_schema in self.indexes.items():
+            self.open_index(index_dir, index_name, index_schema, create=True)
 
     def open_index(self, indexdir, indexname, schema, create=False):
         """
@@ -75,27 +73,27 @@ class WhooshIndex(object):
         """
         try: # open indexes
             index = open_dir(indexdir, indexname=indexname)
-            return index
+            setattr(self, indexname, index)
         except (IOError, OSError, EmptyIndexError) as err:
             if create:
+                self.create_index(indexdir, indexname, schema)
                 try:
-                    os.mkdir(indexdir)
-                except:
-                    # ignore exception, we'll get another exception below
-                    # in case there are problems with the indexdir
-                    pass
-                try:
-                    create_in(indexdir, getattr(self, schema), indexname=indexname)
                     index = open_dir(indexdir, indexname=indexname)
-                    return index
-                except (IOError, OSError) as err:
-                    logging.error(u"%s [while trying to create/open index '%s' in '%s']" % (str(err), indexname, indexdir))
+                    setattr(self, indexname, index)
+                except:
+                    # if we get here, it failed without recovery
+                    raise FatalError("can't open whoosh index")
             else:
-                logging.error(u"%s [while trying to open index '%s' in '%s']" % (str(err), indexname, indexdir))
-            # if we get here, it failed without recovery
-            from MoinMoin.error import FatalError
-            raise FatalError("can't open nor create whoosh index")
+                raise FatalError("can't open whoosh index")
 
-    def rebuild(self):
-        pass
-
+    def create_index(self, indexdir, indexname, schema):
+        try:
+            os.mkdir(indexdir)
+        except:
+            # ignore exception, we'll get another exception below
+            # in case there are problems with the indexdir
+            pass
+        try:
+            create_in(indexdir, getattr(self, schema), indexname=indexname)
+        except (IOError, OSError) as err:
+            logging.error(u"%s [while trying to create index '%s' in '%s']" % (str(err), indexname, indexdir))
