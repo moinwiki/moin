97d0b815b05191edf7b009a082509571f468fe3f
pkumar <contactprashantat@gmail.com>
2011-06-21 01:57:36 +0530

    init_test_app called in setup_method() of TestUsersettings to avoid the test failure in test_frontend.

--- MoinMoin/apps/frontend/_tests/test_frontend.py
+++ MoinMoin/apps/frontend/_tests/test_frontend.py
@@ -12,7 +12,8 @@ from werkzeug import ImmutableMultiDict
 from MoinMoin.apps.frontend import views
 from MoinMoin import user
 from MoinMoin.util import crypto
-
+from MoinMoin.conftest import init_test_app, deinit_test_app
+from MoinMoin._tests import wikiconfig
 import pytest
 
 class TestFrontend(object):
@@ -60,6 +61,8 @@ class TestUsersettings(object):
         flaskg.user = user.User()
 
         self.user = None
+        # temporary hack till we apply some cleanup mechanism in tests
+        init_test_app(wikiconfig.Config)
 
     def teardown_method(self, method):
         """ Run after each test
