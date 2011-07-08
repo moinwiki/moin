7106ab6cbca4da1e8243e9f3c3b8998e2dafa81e
Michael Mayorov <marchael@kb.csu.ru>
2011-07-08 03:26:07 +0000

    Testing and working:
    MoinMoin.storage.backend.indexing
    Add revisions to whoosh indexes
    Remove revisions from whoosh indexes
    Show history for given item or whole indexes
    Show all items for given tag
    Show all tags
    
    MoinMoin.scripts.maint.index:
    Add show function to print all reviosions to stdout for given index
    
    Added new name_exact field to both schemas for strict search
    
    Add index_dir to wikiconfig.py, minor fixes in tests

--- MoinMoin/search/indexing.py
+++ MoinMoin/search/indexing.py
@@ -17,18 +17,12 @@ from MoinMoin.error import FatalError
 from MoinMoin import log
 logging = log.getLogger(__name__)
 
-'''
-for text items, it will be duplication. For "binary" items, it will only store
-what the filter code outputs (e.g. if it is a PDF, it will only store what
-pdftotext outputs as text, if it is a mp3, it will only store title/author/genre 
-[not the whole mp3, of course])
-'''
-
 
 class WhooshIndex(object):
 
     latest_revisions_schema = Schema(wikiname=ID(stored=True),
                                      name=TEXT(stored=True, analyzer=item_name_analyzer()),
+                                     name_exact=ID,
                                      uuid=ID(unique=True, stored=True),
                                      rev_no=NUMERIC(stored=True),
                                      mtime=DATETIME(stored=True),
@@ -46,6 +40,7 @@ class WhooshIndex(object):
 
     all_revisions_schema = Schema(wikiname=ID(stored=True),
                                   name=TEXT(stored=True, analyzer=item_name_analyzer()),
+                                  name_exact=ID,
                                   uuid=ID(stored=True),
                                   rev_no=NUMERIC(stored=True),
                                   mtime=DATETIME(stored=True),
@@ -62,25 +57,25 @@ class WhooshIndex(object):
                'all_revisions_index': 'all_revisions_schema',
               }
 
-    def __init__(self, indexdir=None):
-        indexdir = indexdir or app.cfg.index_dir
+    def __init__(self, index_dir=None):
+        index_dir = index_dir or app.cfg.index_dir
         for index_name, index_schema in self.indexes.items():
-            self.open_index(indexdir, index_name, index_schema, create=True)
+            self.open_index(index_dir, index_name, index_schema, create=True)
 
-    def open_index(self, indexdir, indexname, schema, create=False):
+    def open_index(self, index_dir, indexname, schema, create=False):
         """
-        open index <indexname> in <indexdir>. if opening fails and <create>
+        open index <indexname> in <index_dir>. if opening fails and <create>
         is True, try creating the index and retry opening it afterwards.
         return index object.
         """
         try: # open indexes
-            index = open_dir(indexdir, indexname=indexname)
+            index = open_dir(index_dir, indexname=indexname)
             setattr(self, indexname, index)
         except (IOError, OSError, EmptyIndexError) as err:
             if create:
-                self.create_index(indexdir, indexname, schema)
+                self.create_index(index_dir, indexname, schema)
                 try:
-                    index = open_dir(indexdir, indexname=indexname)
+                    index = open_dir(index_dir, indexname=indexname)
                     setattr(self, indexname, index)
                 except:
                     # if we get here, it failed without recovery
@@ -88,14 +83,14 @@ class WhooshIndex(object):
             else:
                 raise FatalError("can't open whoosh index")
 
-    def create_index(self, indexdir, indexname, schema):
+    def create_index(self, index_dir, indexname, schema):
         try:
-            os.mkdir(indexdir)
+            os.mkdir(index_dir)
         except:
             # ignore exception, we'll get another exception below
-            # in case there are problems with the indexdir
+            # in case there are problems with the index_dir
             pass
         try:
-            create_in(indexdir, getattr(self, schema), indexname=indexname)
+            create_in(index_dir, getattr(self, schema), indexname=indexname)
         except (IOError, OSError) as err:
-            logging.error(u"%s [while trying to create index '%s' in '%s']" % (str(err), indexname, indexdir))
+            logging.error(u"%s [while trying to create index '%s' in '%s']" % (str(err), indexname, index_dir))
