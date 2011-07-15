a4d1f2ed5ad02cc74c73ca9247a2bc44601775cb
pkumar <contactprashantat@gmail.com>
2011-07-15 20:43:06 +0530

    added test to util/test_forms.test_required_filter to test if tagname is 'input'

--- MoinMoin/util/_tests/test_forms.py
+++ MoinMoin/util/_tests/test_forms.py
@@ -10,24 +10,24 @@ from MoinMoin.util import forms
 import pytest
 
 class Bind:
-    """ class for self defined bind attributes """
+    """ class for self defined test_bind attributes """
     def __init__(self):
         self.label = 'test_content'
-        self.default_value = 'bind_default'
+        self.default_value = 'test_bind_default'
         self.optional = True
-        self.properties = 'bind_properties'
-bind = Bind()
+        self.properties = 'test_bind_properties'
+test_bind = Bind()
 
-test_attributes = {'type':'submit', u'class':'test_class'}
+test_attributes = {'type':'submit', u'class':'test_class', u'required':'test_required'}
 
 def test_label_filter():
     # when content is None
-    result1 = forms.label_filter('test_tagname', test_attributes, None, 'test_context', bind)
+    result1 = forms.label_filter('test_tagname', test_attributes, None, 'test_context', test_bind)
     expected = 'test_content'
     assert result1 == expected
 
     # when content is not None
-    result2 = forms.label_filter('test_tagname', test_attributes, 'new_content', 'test_context', bind)
+    result2 = forms.label_filter('test_tagname', test_attributes, 'new_content', 'test_context', test_bind)
     expected = 'new_content'
     assert result2 == expected
 
@@ -37,25 +37,32 @@ def test_button_filter():
     assert result == expected
 
     # attributes.get('type') in ['submit', 'reset', ])
-    content_result = forms.button_filter('input', test_attributes, 'new_content', 'test_context', bind)
+    content_result = forms.button_filter('input', test_attributes, 'new_content', 'test_context', test_bind)
     expected = 'new_content'
     assert content_result == expected 
     attributes_result = test_attributes['value']
-    expected = 'bind_default'
+    expected = 'test_bind_default'
     assert attributes_result == expected
 
     # tagname == 'button'
-    content_result = forms.button_filter('button', test_attributes, None, 'test_context', bind)
-    expected = 'bind_default'
+    content_result = forms.button_filter('button', test_attributes, None, 'test_context', test_bind)
+    expected = 'test_bind_default'
     assert content_result == expected
 
 def test_required_filter():
-    bind.optional = False
-    content_result = forms.required_filter('test_tagname', test_attributes, 'new_content', 'test_context', bind)
+    test_bind.optional = False
+    content_result = forms.required_filter('test_tagname', test_attributes, 'new_content', 'test_context', test_bind)
     expected = 'new_content'
     assert content_result == expected
     attribute_result = test_attributes[u'class']
     expected = u'required'
     assert attribute_result == expected
 
+    # tagname == 'input'
+    content_result = forms.required_filter('input', test_attributes, 'new_content', 'test_context', test_bind)
+    expected = 'new_content'
+    assert content_result == expected
+    attribute_result = test_attributes[u'required']
+    expected = u'required'
+    assert attribute_result == expected
 
