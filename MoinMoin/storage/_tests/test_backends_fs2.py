16355ca7992a7eb154cb73bfd2e4e55e1bfd0a5f
pkumar <contactprashantat@gmail.com>
2011-07-24 12:45:00 +0530

    FS2Backend is sufficient for testing storage.backends.fs2

--- MoinMoin/storage/_tests/test_backends_fs2.py
+++ MoinMoin/storage/_tests/test_backends_fs2.py
@@ -16,7 +16,7 @@ class TestFS2Backend(BackendTest):
 
     def create_backend(self):
         self.tempdir = tempfile.mkdtemp('', 'moin-')
-        return RouterBackend([('/', FS2Backend(self.tempdir))], index_uri='sqlite://')
+        return FS2Backend(self.tempdir)
 
     def kill_backend(self):
         try:
