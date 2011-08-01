bbc785b579c5780af60b4db2af46304851d3d58e
Michael Mayorov <marchael@kb.csu.ru>
2011-08-01 20:57:04 +0000

    fixing multiple heads

--- setup.py
+++ setup.py
@@ -90,7 +90,6 @@ Topic :: Text Processing :: Markup""".splitlines(),
         'Werkzeug==0.6.2', # use this if 0.7dev fails
         'py==1.3.4', # py.test 1.3.4 is needed by unit tests
         'sphinx', # needed to build the docs
-        'whoosh==2.0', # needed for indexed search
         # pin specific versions that conform to XStatic 0.0.2:
         'XStatic==0.0.2',
         'XStatic-CKEditor==3.6.1.2',
