4eb07b261f0dd45e5730b073fbb880c8e40ba66a
TW <tw@waldmann-edv.de>
2011-08-10 11:14:41 +0200

    add missing log/logging import

---
+++ MoinMoin/apps/feed/views.py
@@ -0,0 +1,77 @@
+# Copyright: 2010 MoinMoin:ThomasWaldmann
+# Copyright: 2010 MoinMoin:DiogenesAugusto
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+    MoinMoin - feed views
+
+    This contains all sort of feeds.
+"""
+
+
+from datetime import datetime
+
+from flask import request, Response
+from flask import current_app as app
+from flask import g as flaskg
+
+from werkzeug.contrib.atom import AtomFeed
+
+from MoinMoin import log
+logging = log.getLogger(__name__)
+
+from MoinMoin import wikiutil
+from MoinMoin.i18n import _, L_, N_
+from MoinMoin.apps.feed import feed
+from MoinMoin.config import NAME, ACL, ACTION, ADDRESS, HOSTNAME, USERID, COMMENT
+from MoinMoin.themes import get_editor_info
+from MoinMoin.items import Item
+from MoinMoin.util.crypto import cache_key
+from MoinMoin.util.interwiki import url_for_item
+
+
+@feed.route('/atom/<itemname:item_name>')
+@feed.route('/atom', defaults=dict(item_name=''))
+def atom(item_name):
+    # maybe we need different modes:
+    # - diffs in html don't look great without stylesheet
+    # - full item in html is nice
+    # - diffs in textmode are OK, but look very simple
+    # - full-item content in textmode is OK, but looks very simple
+    cid = cache_key(usage="atom", item_name=item_name)
+    content = app.cache.get(cid)
+    if content is None:
+        title = app.cfg.sitename
+        feed = AtomFeed(title=title, feed_url=request.url, url=request.host_url)
+        for rev in flaskg.storage.history(item_name=item_name):
+            this_rev = rev
+            this_revno = rev.revno
+            item = rev.item
+            name = rev[NAME]
+            try:
+                hl_item = Item.create(name, rev_no=this_revno)
+                previous_revno = this_revno - 1
+                if previous_revno >= 0:
+                    # simple text diff for changes
+                    previous_rev = item.get_revision(previous_revno)
+                    content = hl_item._render_data_diff_text(previous_rev, this_rev)
+                    content = '<div><pre>%s</pre></div>' % content
+                else:
+                    # full html rendering for new items
+                    content = hl_item._render_data()
+                content_type = 'xhtml'
+            except Exception as e:
+                logging.exception("content rendering crashed")
+                content = _(u'MoinMoin feels unhappy.')
+                content_type = 'text'
+            feed.add(title=name, title_type='text',
+                     summary=rev.get(COMMENT, ''), summary_type='text',
+                     content=content, content_type=content_type,
+                     author=get_editor_info(rev, external=True),
+                     url=url_for_item(name, rev=this_revno, _external=True),
+                     updated=datetime.utcfromtimestamp(rev.timestamp),
+                    )
+        content = feed.to_string()
+        app.cache.set(cid, content)
+    return Response(content, content_type='application/atom+xml')
+
---
+++ MoinMoin/storage/_tests/test_middleware_acl.py
@@ -0,0 +1,111 @@
+# Copyright: 2009 MoinMoin:ChristopherDenter
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+    MoinMoin - Test - ACLMiddleWare
+
+    This defines tests for the ACLMiddleWare
+"""
+
+
+import py
+
+from flask import g as flaskg
+
+from MoinMoin.config import ACL
+from MoinMoin.storage.error import AccessDeniedError
+from MoinMoin.storage._tests.test_backends import BackendTest
+from MoinMoin._tests import wikiconfig
+
+
+class TestACLMiddleware(BackendTest):
+    class Config(wikiconfig.Config):
+        content_acl = dict(default=u"All:admin,read,write,destroy,create")
+
+
+    def create_backend(self):
+        # Called before *each* testcase. Provides fresh backends every time.
+        return flaskg.storage
+
+    def kill_backend(self):
+        pass
+
+
+    def get_item(self, name):
+        # Just as a shortcut
+        return flaskg.storage.get_item(name)
+
+    def create_item_acl(self, name, acl):
+        item = flaskg.storage.create_item(name)
+        rev = item.create_revision(0)
+        rev[ACL] = acl
+        item.commit()
+        return item
+
+
+    def test_noaccess(self):
+        name = u"noaccess"
+        self.create_item_acl(name, u"All:")
+        assert py.test.raises(AccessDeniedError, self.get_item, name)
+
+    def test_create_item(self):
+        class Config(wikiconfig.Config):
+            # no create
+            content_acl = dict(default=u"All:admin,read,write,destroy")
+
+        backend = flaskg.storage
+        assert py.test.raises(AccessDeniedError, backend.create_item, u"I will never exist")
+
+        item = self.create_item_acl(u"i will exist!", u"All:read,write")
+        rev = item.create_revision(1)
+        data = "my very existent data"
+        rev.write(data)
+        item.commit()
+        assert item.get_revision(1).read() == data
+
+    def test_read_access_allowed(self):
+        name = u"readaccessallowed"
+        self.create_item_acl(name, u"All:read")
+        # Should simply pass...
+        item = self.get_item(name)
+
+        # Should not...
+        assert py.test.raises(AccessDeniedError, item.create_revision, 1)
+        assert py.test.raises(AccessDeniedError, item.change_metadata)
+
+    def test_write_after_create(self):
+        name = u"writeaftercreate"
+        item = self.create_item_acl(name, u"All:")
+        assert py.test.raises(AccessDeniedError, item.create_revision, 1)
+
+    def test_modify_without_acl_change(self):
+        name = u"copy_without_acl_change"
+        acl = u"All:read,write"
+        self.create_item_acl(name, acl)
+        item = self.get_item(name)
+        rev = item.create_revision(1)
+        # This should pass
+        rev[ACL] = acl
+        item.commit()
+
+    def test_copy_with_acl_change(self):
+        name = u"copy_with_acl_change"
+        acl = u"All:read,write"
+        self.create_item_acl(name, acl)
+        item = self.get_item(name)
+        rev = item.create_revision(1)
+        # without admin rights it is disallowed to change ACL
+        py.test.raises(AccessDeniedError, rev.__setitem__, ACL, acl + u",destroy")
+
+    def test_write_without_read(self):
+        name = u"write_but_not_read"
+        acl = u"All:write"
+        item = flaskg.storage.create_item(name)
+        rev = item.create_revision(0)
+        rev[ACL] = acl
+        rev.write("My name is " + name)
+        item.commit()
+
+        py.test.raises(AccessDeniedError, item.get_revision, -1)
+        py.test.raises(AccessDeniedError, item.get_revision, 0)
+
