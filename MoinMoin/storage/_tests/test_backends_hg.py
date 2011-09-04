3cf028b656db16c75d4749fe95118803e2b3c509
TW <tw@waldmann-edv.de>
2011-09-04 18:27:04 +0200

    comment stuff in hg backend tests which seem to have strange side effects on other storage tests
    (note: i have a current mercurial installed in this virtualenv)
    
    before this change:
    
    MoinMoin/storage/_tests/test_backends_fs.py .............................................................
    MoinMoin/storage/_tests/test_backends_fs19.py .....................
    MoinMoin/storage/_tests/test_backends_fs2.py .............................................................
    MoinMoin/storage/_tests/test_backends_hg.py XXXXXXXXXXXXXXXXXXXxxXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxxXXXxXXXXXXx
    MoinMoin/storage/_tests/test_backends_memory.py XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
    MoinMoin/storage/_tests/test_backends_router.py XXXXXXXXXXXXXXXXxXXXXXXXXXXXXXXXXXXXxxxxXXXxxXXXXXXXXXXXXXX..F......
    MoinMoin/storage/_tests/test_backends_sqla.py XXXXXXXXXXXXXXXXXXXxxXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX..........
    MoinMoin/storage/_tests/test_indexing.py ..s
    MoinMoin/storage/_tests/test_middleware_acl.py XXXXXXXXXXXXXXXXxXXXXXXXXXXXXXXXXXXXxxxxXXXxxXXXXXXXXXXXXXX.F.....
    MoinMoin/storage/_tests/test_serialization.py FFF..
    = 16 failed, 652 passed, 221 skipped, 22 xfailed, 339 xpassed in 81.31 seconds =
    
    after this change:
    
    MoinMoin/storage/_tests/test_backends_fs.py .............................................................
    MoinMoin/storage/_tests/test_backends_fs19.py .....................
    MoinMoin/storage/_tests/test_backends_fs2.py .............................................................
    MoinMoin/storage/_tests/test_backends_hg.py ...................FF................................FF...F......F
    MoinMoin/storage/_tests/test_backends_memory.py ......................................................................................................................
    MoinMoin/storage/_tests/test_backends_router.py ................F...................FFFF...FF................F......
    MoinMoin/storage/_tests/test_backends_sqla.py ...................FF................................................
    MoinMoin/storage/_tests/test_indexing.py ..s
    MoinMoin/storage/_tests/test_middleware_acl.py ................F...................FFFF...FF...............F.....
    MoinMoin/storage/_tests/test_serialization.py FFF..
    ============= 38 failed, 992 passed, 220 skipped in 80.50 seconds ==============

--- MoinMoin/storage/_tests/test_backends_hg.py
+++ MoinMoin/storage/_tests/test_backends_hg.py
@@ -23,7 +23,7 @@ from MoinMoin.storage.backends.hg import MercurialBackend
 from MoinMoin.storage.error import BackendError
 
 class TestMercurialBackend(BackendTest):
-    pytestmark = pytest.mark.xfail(reason='not maintained')
+    #pytestmark = pytest.mark.xfail(reason='not maintained')
 
     def create_backend(self):
         self.test_dir = mkdtemp()
