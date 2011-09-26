8d71a5b9377cecf91eed6145e0d97d2574ab5541
TW <tw@waldmann-edv.de>
2011-09-26 13:08:03 +0200

    fix some tests, src cosmetics

--- MoinMoin/apps/feed/_tests/test_feed.py
+++ MoinMoin/apps/feed/_tests/test_feed.py
@@ -1,12 +1,19 @@
-# Copyright: 2010 MoinMoin:ThomasWaldmann
+# Copyright: 2010,2011 MoinMoin:ThomasWaldmann
 # License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
 
 """
-    MoinMoin - basic tests for feeds
+MoinMoin - basic tests for feeds
 """
+
+from MoinMoin._tests import wikiconfig
 
 
 class TestFeeds(object):
+    class Config(wikiconfig.Config):
+        """
+        we just have this so the test framework creates a new app with empty backends for us.
+        """
+
     def test_global_atom(self):
         with self.app.test_client() as c:
             rv = c.get('/+feed/atom')
