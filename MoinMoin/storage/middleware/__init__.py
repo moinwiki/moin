1d5d0fb8dab5558437186a37f82332046b14b935
Bruno Martin <bruno@hacklab.com.br>
2011-10-27 20:05:16 -0200

    fixes #54, global index inherited from wrong parent template

---
+++ MoinMoin/storage/middleware/__init__.py
@@ -0,0 +1,9 @@
+# Copyright: 2011 MoinMoin:ThomasWaldmann
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+MoinMoin - misc. middleware
+
+Middleware sits either on a backend or on another middleware.
+"""
+
