cacb4fbdc2c7e4baf2ea796edcef57daee3aa69b
Sam Toyer <samATqxcvDOTnet@localhost>
2011-11-26 00:29:16 +1000

    Unroll test parameterisation, they all run now.

--- MoinMoin/apps/admin/_tests/test_admin.py
+++ MoinMoin/apps/admin/_tests/test_admin.py
@@ -7,21 +7,7 @@
 
 from flask import url_for
 
-def pytest_generate_tests(metafunc):
-    if 'view' in metafunc.funcargs:
-        parameters = metafunc.cls.params[metafunc.func.__name__]
-        argnames = parameters[0].keys()
-        metafunc.parametrize(argnames, [[parameters[argument] for argument in argnames] for parameter in parameters])
-
 class TestAdmin(object):
-    superuser_views = ['admin.' + action for action in ['require_permission', 'sysitems_upgrade',
-            'wikiconfig', 'wikiconfighelp',
-            ]
-        ]
-    params = {
-        'test_superuser_page': [dict(view=view) for view in superuser_views]
-        }
-
     def _test_view_get(self, viewname, status='200 OK', data=['<html>', '</html>'], viewopts={}):
         with self.app.test_client() as c:
             rv = c.get(url_for(viewname, **viewopts))
@@ -35,9 +21,11 @@ class TestAdmin(object):
     def test_userprofile(self):
         self._test_view_get('admin.userprofile', status='403 FORBIDDEN', viewopts=dict(user_name='DoesntExist'))
 
-    def test_superuser_page(self, view):
-        # This will die for the reasons outlined in apps/frontend/_tests/test_frontend.py
-        self._test_view_get(view, status='403 FORBIDDEN')
+    def test_wikiconfig(self):
+        self._test_view_get('admin.wikiconfig', status='403 FORBIDDEN')
+
+    def test_wikiconfighelp(self):
+        self._test_view_get('admin.wikiconfighelp', status='403 FORBIDDEN')
 
     def test_interwikihelp(self):
         self._test_view_get('admin.interwikihelp')
