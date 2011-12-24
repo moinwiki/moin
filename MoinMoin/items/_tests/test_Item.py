92b0c64e4a7d12bb9c4be09d0724863ac7e94181
Bilal Akhtar <bilalakhtar@ubuntu.com>
2011-12-24 12:03:56 -0500

    change assert statement in such a way so that it succeeds with both older and newer (0.8.2+) versions of werkzeug

--- MoinMoin/items/_tests/test_Item.py
+++ MoinMoin/items/_tests/test_Item.py
@@ -401,7 +401,9 @@ class TestTransformableBitmapImage(object):
         try:
             from PIL import Image as PILImage
             result = TransformableBitmapImage._render_data_diff(item1, item.rev, item1.rev)
-            assert str(result).startswith('<img src="/%2Bdiffraw/image_Item?rev')
+            # On Werkzeug 0.8.2+, urls with '+' are automatically encoded to '%2B'
+            assert [str(result).startswith('<img src="/+diffraw/image_Item?rev'),
+                    str(result).startswith('<img src="/%2Bdiffraw/image_Item?rev')]
         except ImportError:
             # no PIL
             pass
