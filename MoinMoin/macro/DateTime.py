598c1b67ef04ab9e0af68d59c4157710f6679011
Bilal Akhtar <bilalakhtar@ubuntu.com>
2011-12-28 13:44:03 -0500

    Catch AccessDenied exceptions while delete()ing or destroy()ing, and show an HTTP 403 error page instead of a traceback

---
+++ MoinMoin/macro/DateTime.py
@@ -0,0 +1,25 @@
+# Copyright: 2008-2011 MoinMoin:ThomasWaldmann
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+MoinMoin DateTime macro - outputs the date and time for some specific point in time,
+adapted to the TZ settings of the user viewing the content.
+"""
+
+import time
+from datetime import datetime
+
+from flask import g as flaskg
+from flaskext.babel import format_datetime
+
+from MoinMoin.macro.Date import MacroDateTimeBase
+
+class Macro(MacroDateTimeBase):
+    def macro(self, content, arguments, page_url, alternative):
+        if arguments is None:
+            tm = time.time() # always UTC
+        else:
+            stamp = arguments[0]
+            tm = self.parse_time(stamp)
+        return format_datetime(datetime.utcfromtimestamp(tm))
+
---
+++ MoinMoin/storage/stores/_tests/test_all.py
@@ -0,0 +1,67 @@
+# Copyright: 2011 MoinMoin:RonnyPfannschmidt
+# Copyright: 2011 MoinMoin:ThomasWaldmann
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+MoinMoin - store tests
+"""
+
+
+from __future__ import absolute_import, division
+
+import pytest
+
+def test_getitem_raises(store):
+    with pytest.raises(KeyError):
+        store['doesnotexist']
+
+
+def test_setitem_getitem_delitem(store):
+    k, v = 'key', 'value'
+    store[k] = v
+    assert v == store[k]
+    del store[k]
+    with pytest.raises(KeyError):
+        store[k]
+
+def test_setitem_getitem_delitem_binary(store):
+    k, v = 'key', '\000\001\002'
+    store[k] = v
+    assert v == store[k]
+    assert len(v) == 3
+    del store[k]
+    with pytest.raises(KeyError):
+        store[k]
+
+
+def test_iter(store):
+    kvs = set([('1', 'one'), ('2', 'two'), ('3', 'three'), ])
+    for k, v in kvs:
+        store[k] = v
+    result = set()
+    for k in store:
+        result.add((k, store[k]))
+    assert result == kvs
+
+
+def test_len(store):
+    assert len(store) == 0
+    store['foo'] = 'bar'
+    assert len(store) == 1
+    del store['foo']
+    assert len(store) == 0
+
+
+def test_perf(store):
+    # XXX: introduce perf test option
+    pytest.skip("usually we do no performance tests")
+    for i in range(1000):
+        key = value = str(i)
+        store[key] = value
+    for i in range(1000):
+        key = expected_value = str(i)
+        assert store[key] == expected_value
+    for i in range(1000):
+        key = str(i)
+        del store[key]
+
