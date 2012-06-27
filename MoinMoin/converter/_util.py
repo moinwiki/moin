7204c56cd756885569c82531ece1e860a66821a3
Cheer Xiao <xiaqqaix@gmail.com>
2012-06-28 00:36:14 +0800

    Cleanups and fixes for the items module.
    * Items member methods are refactored, esp. modify() and do_modify. The
      motivation is that the family of methods whose names are single verbs -
      rename(), delete(), revert(), destroy(), modify() to be request-agnostic,
      which makes the code more testable and consistent. Codes in modify() that
      dealt with requests are moved to do_modify() and other places.
    
    * Template support is fixed and enabled for *Draw items due to the refactor.

---
+++ MoinMoin/converter/_util.py
@@ -0,0 +1,50 @@
+# Copyright: 2011 MoinMoin:ThomasWaldmann
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+MoinMoin - converter utilities
+"""
+
+
+from __future__ import absolute_import, division
+
+from MoinMoin.config import uri_schemes
+from MoinMoin.util.iri import Iri
+from MoinMoin.util.mime import Type
+
+def allowed_uri_scheme(uri):
+    parsed = Iri(uri)
+    return not parsed.scheme or parsed.scheme in uri_schemes
+
+def decode_data(data, contenttype=None):
+    """
+    read and decode data, return unicode text
+
+    supported types for data:
+    - rev object
+    - str
+    - unicode
+
+    file-like objects and str need to be either utf-8 (or ascii, which is a subset of utf-8)
+    encoded or contenttype (including a charset parameter) needs to be given.
+    """
+    if not isinstance(data, (str, unicode)):
+        data = data.data.read()
+    if isinstance(data, str):
+        coding = 'utf-8'
+        if contenttype is not None:
+            ct = Type(contenttype)
+            coding = ct.parameters.get('charset', coding)
+        data = data.decode(coding)
+    if not isinstance(data, unicode):
+        raise TypeError("data must be rev or str (requires contenttype with charset) or unicode, but we got {0!r}".format(data))
+    return data
+
+
+def normalize_split_text(text):
+    """
+    normalize line endings, split text into a list of lines
+    """
+    text = text.replace(u'\r\n', u'\n')
+    lines = text.split(u'\n')
+    return lines
---
+++ MoinMoin/storage/middleware/routing.py
@@ -0,0 +1,116 @@
+# Copyright: 2008-2011 MoinMoin:ThomasWaldmann
+# Copyright: 2011 MoinMoin:RonnyPfannschmidt
+# Copyright: 2009 MoinMoin:ChristopherDenter
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+MoinMoin - routing middleware
+
+Routes requests to different backends depending on the item name.
+
+Just think of UNIX filesystems, fstab and mount.
+
+This middleware lets you mount backends that store items belonging to some
+specific part of the namespace. Routing middleware has same API as a backend.
+"""
+
+
+from __future__ import absolute_import, division
+
+from MoinMoin.config import NAME
+
+from MoinMoin.storage.backends import BackendBase, MutableBackendBase
+
+
+class Backend(MutableBackendBase):
+    """
+    router, behaves readonly for readonly mounts
+    """
+    def __init__(self, mapping):
+        """
+        Initialize router backend.
+
+        The mapping given must satisfy the following criteria:
+            * Order matters.
+            * Mountpoints are just item names, including the special '' (empty)
+              root item name.
+            * Trailing '/' of a mountpoint will be stripped.
+            * There *must* be a backend with mountpoint '' at the very
+              end of the mapping. That backend is then used as root, which means
+              that all items that don't lie in the namespace of any other
+              backend are stored there.
+
+        :type mapping: list of tuples of mountpoint -> backend mappings
+        :param mapping: [(mountpoint, backend), ...]
+        """
+        self.mapping = [(mountpoint.rstrip('/'), backend) for mountpoint, backend in mapping]
+
+    def open(self):
+        for mountpoint, backend in self.mapping:
+            backend.open()
+
+    def close(self):
+        for mountpoint, backend in self.mapping:
+            backend.close()
+
+    def _get_backend(self, itemname):
+        """
+        For a given fully-qualified itemname (i.e. something like Company/Bosses/Mr_Joe)
+        find the backend it belongs to (given by this instance's mapping), the local
+        itemname inside that backend and the mountpoint of the backend.
+
+        :param itemname: fully-qualified itemname
+        :returns: tuple of (backend, local itemname, mountpoint)
+        """
+        for mountpoint, backend in self.mapping:
+            if itemname == mountpoint or itemname.startswith(mountpoint and mountpoint + '/' or ''):
+                lstrip = mountpoint and len(mountpoint)+1 or 0
+                return backend, itemname[lstrip:], mountpoint
+        raise AssertionError("No backend found for {0!r}. Available backends: {1!r}".format(itemname, self.mapping))
+
+    def __iter__(self):
+        # Note: yields <backend_mountpoint>/<backend_revid> as router revid, so that this
+        #       can be given to get_revision and be routed to the right backend.
+        for mountpoint, backend in self.mapping:
+            for revid in backend:
+                yield (mountpoint, revid)
+
+    def retrieve(self, name, revid):
+        backend, _, mountpoint = self._get_backend(name)
+        meta, data = backend.retrieve(revid)
+        if mountpoint:
+            name = meta[NAME]
+            if name:
+                meta[NAME] = u'{0}/{1}'.format(mountpoint, meta[NAME])
+            else:
+                meta[NAME] = mountpoint # no trailing slash!
+        return meta, data
+
+    # writing part
+    def create(self):
+        for mountpoint, backend in self.mapping:
+            if isinstance(backend, MutableBackendBase):
+                backend.create()
+            #XXX else: log info?
+
+    def destroy(self):
+        for mountpoint, backend in self.mapping:
+            if isinstance(backend, MutableBackendBase):
+                backend.destroy()
+            #XXX else: log info?
+
+    def store(self, meta, data):
+        mountpoint_itemname = meta[NAME]
+        backend, itemname, mountpoint = self._get_backend(mountpoint_itemname)
+        if not isinstance(backend, MutableBackendBase):
+            raise TypeError('backend {0!r} mounted at {1!r} is readonly'.format(backend, mountpoint))
+        meta[NAME] = itemname
+        revid = backend.store(meta, data)
+        meta[NAME] = mountpoint_itemname # restore the original name
+        return revid
+
+    def remove(self, name, revid):
+        backend, _, mountpoint = self._get_backend(name)
+        if not isinstance(backend, MutableBackendBase):
+            raise TypeError('backend {0!r} mounted at {1!r} is readonly'.format(backend, mountpoint))
+        backend.remove(revid)
