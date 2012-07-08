f84921007eef3d31a8aeba8157d9ef054b8b5bdc
Ivan Gavrilov <ivan.wntr@gmail.com>
2012-07-08 14:13:13 +0400

    One more fix for unicode issues. This one resolves a case with renaming an image page (create non-unicode image page, rename it with unicode characters).

---
+++ MoinMoin/auth/_tests/test_http.py
@@ -0,0 +1,43 @@
+# Copyright: 2011 Prashant Kumar <contactprashantat AT gmail DOT com>
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+Test for auth.http
+"""
+
+from flask import g as flaskg
+from flask import request
+
+from MoinMoin.user import create_user
+from MoinMoin.auth.http import HTTPAuthMoin
+import pytest
+
+class TestHTTPAuthMoin(object):
+    """ Test: HTTPAuthMoin """
+    class Auth:
+        def __init__(self):
+            self.username = 'ValidUser'
+            self.password = 'test_pass'
+
+    def setup_method(self, method):
+        flaskg.user.auth_method = 'http'
+        request.authorization = self.Auth()
+
+    def teardown_method(self, method):
+        flaskg.user.auth_method = 'invalid'
+        request.authorization = None
+
+    def test_request(self):
+        # create a new user
+        create_user(u'ValidUser', u'test_pass', u'test_email@moinmoin')
+        httpauthmoin_obj = HTTPAuthMoin()
+        test_user, bool_val = httpauthmoin_obj.request(flaskg.user)
+        assert test_user.valid
+        assert test_user.name == u'ValidUser'
+        assert bool_val
+
+        # when auth_method is not 'http'
+        flaskg.user.auth_method = 'invalid'
+        test_user, bool_val = httpauthmoin_obj.request(flaskg.user)
+        assert not test_user.valid
+        assert test_user.name == u'anonymous'
---
+++ MoinMoin/storage/stores/__init__.py
@@ -0,0 +1,126 @@
+# Copyright: 2011 MoinMoin:RonnyPfannschmidt
+# Copyright: 2011 MoinMoin:ThomasWaldmann
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+MoinMoin - simple key/value stores.
+
+If some kvstore implementation you'ld like to use is missing from this package,
+you can likely implement it adding very little and rather easy code.
+"""
+
+
+from __future__ import absolute_import, division
+
+import io
+from abc import abstractmethod
+from collections import Mapping, MutableMapping
+
+
+class StoreBase(Mapping):
+    """
+    A simple read-only key/value store.
+    """
+    @classmethod
+    @abstractmethod
+    def from_uri(cls, uri):
+        """
+        return an instance constructed from the given uri
+        """
+
+    def __init__(self, **kw):
+        """
+        lazy stuff - just remember pathes, urls, database name, ... -
+        whatever we need for open(), create(), etc.
+        """
+
+    def open(self):
+        """
+        open the store, prepare it for usage
+        """
+
+    def close(self):
+        """
+        close the store, stop using it, free resources (except stored data)
+        """
+
+    @abstractmethod
+    def __iter__(self):
+        """
+        iterate over keys present in the store
+        """
+
+    def __len__(self):
+        return len([key for key in self])
+
+    @abstractmethod
+    def __getitem__(self, key):
+        """
+        return data stored for key
+        """
+
+
+class BytesStoreBase(StoreBase):
+    @abstractmethod
+    def __getitem__(self, key):
+        """
+        return bytestring for key if exists else raise KeyError
+        """
+
+
+class FileStoreBase(StoreBase):
+    @abstractmethod
+    def __getitem__(self, key):
+        """
+        return a filelike for key if exists else raise KeyError
+
+        note: the caller is responsible for closing the open file we return
+              after usage.
+        """
+
+
+class MutableStoreBase(StoreBase, MutableMapping):
+    """
+    A simple read/write key/value store.
+    """
+    def create(self):
+        """
+        create an empty store
+        """
+
+    def destroy(self):
+        """
+        destroy the store (erase all stored data, remove store)
+        """
+
+    @abstractmethod
+    def __setitem__(self, key, value):
+        """
+        store value under key
+        """
+
+    @abstractmethod
+    def __delitem__(self, key):
+        """
+        delete the key, dereference the related value in the store
+        """
+
+
+class BytesMutableStoreBase(MutableStoreBase):
+    @abstractmethod
+    def __setitem__(self, key, value):
+        """
+        store a bytestring for key
+        """
+
+
+class FileMutableStoreBase(MutableStoreBase):
+    @abstractmethod
+    def __setitem__(self, key, stream):
+        """
+        store a filelike for key
+
+        note: caller is responsible for giving us a open file AND also for
+              closing that file later. caller must not rely on some specific
+              file pointer position after we return.
+        """
