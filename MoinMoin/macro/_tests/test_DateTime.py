247730a7b81a9fce94351881ff60527ef245200e
TW <tw@waldmann-edv.de>
2012-07-28 22:58:04 +0200

    keys must be str, not unicode - they are used for python argument names and 2.6 wants str there

---
+++ MoinMoin/macro/_tests/test_DateTime.py
@@ -0,0 +1,32 @@
+# Copyright: 2011 Prashant Kumar <contactprashantat AT gmail DOT com>
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+Test for macro.DateTime
+"""
+
+import time
+from datetime import datetime
+
+from MoinMoin.macro.DateTime import *
+import pytest
+
+def test_Macro():
+    """Test: DateTime.Macro """
+
+    macro_obj = Macro()
+    # when arguments is None
+    result = macro_obj.macro('content', None, 'page_url', 'alternative')
+    # get the current time
+    test_time = time.time()
+    test_time = format_datetime(datetime.utcfromtimestamp(test_time))
+    assert test_time == result
+
+    arguments = ['2011-08-07T11:11:11', 'argument2']
+    result = macro_obj.macro('content', arguments, 'page_url', 'alternative')
+    expected = u'Aug 7, 2011 11:11:11 AM'
+    assert result == expected
+
+    arguments = ['incorrect_argument']
+    with pytest.raises(ValueError):
+        macro_obj.macro('content', arguments, 'page_url', 'alternative')
