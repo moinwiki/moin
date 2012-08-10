fc8a5f8163a3146d0579bf4a2ad831bed411a5a6
Pavel Sviderski <pavel@psviderski.name>
2012-08-10 14:36:12 +0200

    Add the ability to create new Blog and Blog entry items.

---
+++ MoinMoin/apps/misc/_tests/test_misc.py
@@ -0,0 +1,31 @@
+# Copyright: 2010,2011 MoinMoin:ThomasWaldmann
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+MoinMoin - basic tests for misc views
+"""
+
+from flask import url_for
+
+from MoinMoin._tests import wikiconfig
+
+class TestMisc(object):
+    class Config(wikiconfig.Config):
+        """
+        we just have this so the test framework creates a new app with empty backends for us.
+        """
+
+    def test_global_sitemap(self):
+        with self.app.test_client() as c:
+            rv = c.get(url_for('misc.sitemap'))
+            assert rv.status == '200 OK'
+            assert rv.headers['Content-Type'] == 'text/xml; charset=utf-8'
+            assert rv.data.startswith('<?xml')
+            assert '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' in rv.data
+            assert '</urlset>' in rv.data
+
+    def test_urls_names(self):
+        with self.app.test_client() as c:
+            rv = c.get(url_for('misc.urls_names'))
+            assert rv.status == '200 OK'
+            assert rv.headers['Content-Type'] == 'text/plain; charset=utf-8'
