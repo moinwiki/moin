6bb30ddd8d019ce1db12fa25e700129eba473b04
Cheer Xiao <xiaqqaix@gmail.com>
2012-08-18 13:51:58 +0800

    Tag some <label>s with css class "moin-inline-label" and make them inline.
    Default style for <label> changed to display:block at some point, but inline
    labels are still required in some places.
    
    The css files have been regnerated from stylus sources.

---
+++ MoinMoin/converter/_tests/test__args_wiki.py
@@ -0,0 +1,40 @@
+# Copyright: 2008 MoinMoin:BastianBlank
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+MoinMoin - Tests for MoinMoin.converter._args_wiki
+"""
+
+
+import pytest
+
+from MoinMoin.converter._args_wiki import *
+
+def test():
+    yield (do,
+        ur'both positional both=foo keyword=bar',
+        [u'both', u'positional'],
+        {u'both': u'foo', u'keyword': u'bar'})
+
+    yield (do,
+        ur'a-b a_b a-c=foo a_c=bar',
+        [u'a-b', u'a_b'],
+        {u'a-c': u'foo', u'a_c': u'bar'})
+
+    yield (do,
+        ur'''"a b\tc\nd" k="a b\tc\nd"''',
+        [u'a b\tc\nd'],
+        {u'k': u'a b\tc\nd'})
+
+def test_parse():
+    a = parse(ur''''a b\tc\nd',k="a b\tc\nd"''')
+    assert a.positional == [u'a b\tc\nd']
+    assert a.keyword == {u'k': u'a b\tc\nd'}
+
+def do(wiki, positional, keyword):
+    a = parse(wiki)
+    assert a.positional == positional
+    assert a.keyword == keyword
+
+    s = unparse(Arguments(positional, keyword))
+    assert s == wiki
