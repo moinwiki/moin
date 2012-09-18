9f20eb2b89bed20ac3f02e23f92bc430102cbede
Hugh Perkins <hughperkins@gmail.com>
2012-09-18 21:29:28 +0800

    created a first selenium test, specifically of adding subitems to an item

---
+++ _ui_tests/test_subitems.py
@@ -0,0 +1,69 @@
+# Copyright: 2012 MoinMoin:HughPerkins
+# License: GNU GPL v3 (or any later version), see LICENSE.txt for details.
+
+"""Functional test: create subitem"""
+
+from selenium import webdriver
+
+import config
+import utils
+
+
+class TestSubitems(object):
+    """Functional test: create subitem"""
+
+    def setup_class(self):
+        """opens browser and creates some random item names for these tests"""
+        self.driver = utils.create_browser()
+        self.base_url = config.BASE_URL
+        self.base_item_name = u"page_" + utils.generate_random_word(5)
+        self.subitem_name = u"subitem_" + utils.generate_random_word(5)
+
+    def create_wiki_item(self, item_name):
+        """Creates a new wiki item with name 'item_name'"""
+        driver = self.driver
+
+        driver.get(self.base_url + u"/" + item_name)
+        driver.find_element_by_link_text(u"Default").click()
+        driver.find_element_by_link_text(u"Wiki (MoinMoin)").click()
+        driver.find_element_by_link_text(
+            u"create the item from scratch").click()
+        driver.find_element_by_id("f_content_form_data_text").send_keys(
+            u"This is a test item\n")
+        driver.find_element_by_id("f_submit").click()
+
+    def test_createsubitem(self):
+        """Test create subitem"""
+        driver = self.driver
+
+        self.create_wiki_item(self.base_item_name)
+
+        driver.get(self.base_url + u"/" + self.base_item_name)
+        driver.find_element_by_link_text(u"Modify").click()
+        driver.find_element_by_id(u"f_content_form_data_text").send_keys(
+            u"\n[[/" + self.subitem_name + "]]\n")
+        driver.find_element_by_id(u"f_submit").click()
+        driver.find_element_by_link_text(u"/" + self.subitem_name).click()
+        driver.find_element_by_link_text(u"Default").click()
+        driver.find_element_by_link_text(u"Wiki (MoinMoin)").click()
+        driver.find_element_by_link_text(u"create the item from scratch").click()
+        driver.find_element_by_id(u"f_content_form_data_text").send_keys(
+            u"This is a test subitem")
+        driver.find_element_by_id(u"f_submit").click()
+        assert u"This is a test subitem" in driver.find_element_by_id(
+            u"moin-content-data").text
+        assert driver.title.split(u" - ")[0] == self.base_item_name + \
+            u"/" + self.subitem_name
+
+    def teardown_class(self):
+        """shuts down browser"""
+        self.driver.quit()
+
+if __name__ == u'__main__':
+    # This lets us run the test directly, without using py.test
+    # This is useful for example for being able to call help, eg
+    # 'help(driver)', or 'help(driver.find_element_by_id("f_submit"))'
+    testSubitems = TestSubitems()
+    testSubitems.setup_class()
+    testSubitems.test_createsubitem()
+    testSubitems.teardown_class()
---
+++ _ui_tests/utils.py
@@ -0,0 +1,43 @@
+# Copyright: 2012 MoinMoin:LiHaiyan
+# Copyright: 2012 MoinMoin:HughPerkins
+# License: GNU GPL v3 (or any later version), see LICENSE.txt for details.
+
+"""Functions to facilitate functional testing"""
+
+import random
+from selenium import webdriver
+import driver_register
+
+
+def create_browser():
+    """
+    Instantiates a firefox browser object, and configures it for English language
+    and registers it for screenshots, and sets the timeout
+    """
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("intl.accept_languages", "en")
+    driver = webdriver.Firefox(firefox_profile=profile)
+    driver_register.register_driver(driver) # register with
+    # driver_register, which is needed so that printscreen on test
+    # failure works
+    driver.implicitly_wait(20)
+    return driver
+
+
+def generate_random_word(length):
+    """
+    generates a random string containing numbers, of length 'length'
+    """
+    word = unicode(random.randint(10**(length-1), 10**length))
+    return word
+
+
+def generate_random_name(prefix, totallength):
+    """
+    create a random name, starting with 'prefix'
+    of total length 'totallength'
+    """
+    length = totallength - len(prefix)
+    numberword = generate_random_word(length)
+    name = prefix + numberword
+    return name
