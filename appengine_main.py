cbef5c419a6a746ec9a62775355e9131540e448e
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2012-09-21 23:05:41 +0200

    make moin work for GAE (google app engine), details see below
    Most of this stuff is based on work done by Guido van Rossum, thanks!
    
    added a GAE store implementation for moin
    
    wikiconfig: adjusted so it works with GAE by default (in this branch):
    * use moin's GAE store
    * use whoosh's GAE backend
    
    logging: currently needs a hack (configured=True), otherwise it just hangs
    strangely.
    
    added a app.yaml definition file for GAE
    
    added a appengine_main.py script for usage with GAE
    
    Notes:
    * there needs to be a "support" directory with all of moin's dependencies
    (except jinja2, sphinx, py.test) on the same level as "MoinMoin" == in the top
    level directory.
    
    * we currently need the 2.5 development version of whoosh.
    
    * I did not apply the patch for filesys.py as we currently do not import
    this module at all, so there is no problem with it on GAE either.

---
+++ appengine_main.py
@@ -0,0 +1,23 @@
+"""Main entry point for Google App Engine."""
+
+# Python imports.
+import os
+import sys
+
+# Configuration constants.
+wiki_config = 'wikiconfig.py'
+
+# Tweak sys.path.
+support_path = os.path.normpath(os.path.join(os.path.dirname(__file__), 'support'))
+if support_path not in sys.path:
+    sys.path.insert(0, support_path)
+
+# Now we can import MoinMoin.
+from MoinMoin.app import create_app
+
+# Hack: If there are no DatastoreFile instances assume we must create the index.
+from whoosh.filedb.gae import DatastoreFile
+create_index = DatastoreFile.all().get() is None
+
+# Create the WSGI application object.
+application = create_app(os.path.abspath(wiki_config), create_index)
