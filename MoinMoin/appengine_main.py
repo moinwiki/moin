bd38eee204a02ded7de1a21b9ce564a9e8169739
TW <tw@waldmann-edv.de>
2012-09-22 00:24:45 +0200

    move appengine_main module to MoinMoin package
    this way, the toplevel directory is less cluttered.

--- MoinMoin/appengine_main.py
+++ MoinMoin/appengine_main.py
@@ -1,20 +1,16 @@
 """Main entry point for Google App Engine."""
 
-# Python imports.
 import os
 import sys
 
-# Tweak sys.path.
-support_path = os.path.normpath(os.path.join(os.path.dirname(__file__), 'support'))
+support_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', 'support'))
 if support_path not in sys.path:
     sys.path.insert(0, support_path)
-
-# Now we can import MoinMoin.
-from MoinMoin.app import create_app
 
 # Hack: If there are no DatastoreFile instances assume we must create the index.
 from whoosh.filedb.gae import DatastoreFile
 create_index = DatastoreFile.all().get() is None
 
 # Create the WSGI application object.
+from MoinMoin.app import create_app
 application = create_app(create_index=create_index)
