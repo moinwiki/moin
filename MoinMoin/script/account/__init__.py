c971629569f2270bb28a8a8c542652ab4961d023
Cheer Xiao <xiaqqaix@gmail.com>
2012-10-01 23:38:17 +0800

    Make RegistryContent.register accept an Entry and group name for slightly cleaner code.

---
+++ MoinMoin/script/account/__init__.py
@@ -0,0 +1,6 @@
+# Copyright: 2006,2011 MoinMoin:ThomasWaldmann
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+    MoinMoin - User Accounts Management Scripts
+"""
