c7a35309816e455a670d180e92244c3d042018e8
Cheer Xiao <xiaqqaix@gmail.com>
2012-10-04 00:13:22 +0800

    Demote priority in registry entries so that it is only compared when other fields are equal.

---
+++ MoinMoin/constants/itemtypes.py
@@ -0,0 +1,18 @@
+# Copyright: 2012 MoinMoin:CheerXiao
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+MoinMoin - itemtype related constants
+"""
+
+
+from __future__ import absolute_import, division
+
+
+ITEMTYPE_DEFAULT = u'default'
+ITEMTYPE_USERPROFILE = u'userprofile'
+ITEMTYPE_NONEXISTENT = u'nonexistent'
+
+ITEMTYPE_TICKET = u'ticket'
+ITEMTYPE_BLOG = u'blog'
+ITEMTYPE_BLOGENTRY = u'blogentry'
