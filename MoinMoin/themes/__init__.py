b984c173983c47d8b65175ce7225a2b7399d2a04
TW <tw@waldmann-edv.de>
2012-11-03 03:32:38 +0100

    fix name dummy type (use unicode)

--- MoinMoin/themes/__init__.py
+++ MoinMoin/themes/__init__.py
@@ -418,7 +418,7 @@ def setup_jinja_env():
                             'storage': flaskg.storage,
                             'clock': flaskg.clock,
                             'cfg': app.cfg,
-                            'item_name': 'handlers need to give it',
+                            'item_name': u'handlers need to give it',
                             'url_for_item': url_for_item,
                             'get_editor_info': lambda meta: get_editor_info(meta),
                             'utctimestamp': lambda dt: utctimestamp(dt),
