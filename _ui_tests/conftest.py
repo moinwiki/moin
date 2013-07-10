c4e8a9790099b8c257fd9a7d9cd36fd6d0c00525
Pavel Sviderski <pavel@psviderski.name>
2013-07-10 22:51:38 +0400

    Fix broken item.modify (filter out None values from the update_meta dict)

---
+++ _ui_tests/conftest.py
@@ -0,0 +1,32 @@
+# Copyright: 2012 MoinMoin:HughPerkins
+# License: GNU GPL v3 (or any later version), see LICENSE.txt for details.
+
+"""
+Contains events called by py.test during the life-cycle of the test suite
+This module is automatically loaded by py.test, which looks for a file
+of this name
+"""
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+import driver_register
+
+
+def pytest_runtest_makereport(item, call):
+    """
+    Entry point for event which occurs after each test has run
+    The parameters are:
+    - item: the method called
+    - call: an object of type CallInfo, which has two properties, of which
+      excinfo contains info about any exception that got thrown by the method
+    This method is called automatically by py.test.  The name of the method
+    is used by py.test to locate it, and decide when to call it
+    This specific method instance is used to take a screenshot whenever a test
+    fails, ie whenever the method throws an exception
+    """
+    if call.excinfo is not None:
+        if driver_register.get_driver() is not None and hasattr(item, 'obj'):
+            driver_register.get_driver().get_screenshot_as_file(
+                unicode(item.obj).split(u" ")[2] + u'.png')
---
+++ contrib/wsgi/proxy.py
@@ -0,0 +1,37 @@
+"""
+    WSGI Middleware for running WSGI apps behind proxies.
+
+    Analyzes HTTP-X-Forwarded-For header and uses the address our outermost
+    trusted proxy is seeing as REMOTE_ADDR.
+
+    @copyright: 2008 MoinMoin:FlorianKrupicka
+    @license: GNU GPL, see COPYING for details.
+"""
+
+
+class ProxyTrust(object):
+    """
+    Middleware that rewrites the remote address according to trusted
+    proxies in the forward chain.
+    """
+
+    def __init__(self, app, proxies):
+        self.app = app
+        self.proxies = proxies
+
+    def __call__(self, environ, start_response):
+        if 'HTTP_X_FORWARDED_FOR' in environ:
+            addrs = environ.pop('HTTP_X_FORWARDED_FOR').split(',')
+            addrs = [addr.strip() for addr in addrs]
+        elif 'REMOTE_ADDR' in environ:
+            addrs = [environ['REMOTE_ADDR']]
+        else:
+            addrs = [None]
+        result = [addr for addr in addrs if addr not in self.proxies]
+        if result:
+            environ['REMOTE_ADDR'] = result[-1]
+        elif addrs[-1] is not None:
+            environ['REMOTE_ADDR'] = addrs[-1]
+        else:
+            del environ['REMOTE_ADDR']
+        return self.app(environ, start_response)
