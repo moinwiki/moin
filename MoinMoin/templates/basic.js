d55c99b1678a782a8fd5778eb016cba55d0d475d
sharky93 <rishabhr123@gmail.com>
2013-07-26 00:27:36 +0530

    fixes issue #14, adds a jquery textarea auto-resize plugin

--- MoinMoin/templates/basic.js
+++ MoinMoin/templates/basic.js
@@ -1,4 +1,5 @@
 $(document).ready(function (){
     // this depends on the id's used for different tab-panes in modify.html (Basic Theme)
     $('#meta, #help').removeClass('active');
+    $('#f_content_form_data_text, #f_extra_meta_text').autosize();
 });
