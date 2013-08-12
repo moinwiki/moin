7ff67bbb5551421d62e3b12054fa4bb992c90078
Ana Balica <ana.balica@gmail.com>
2013-08-12 21:31:06 +0300

    pass info to the receivers of item_modified signal

---
+++ MoinMoin/util/notifications.py
@@ -0,0 +1,10 @@
+# Copyright: 2013 MoinMoin:AnaBalica
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+    MoinMoin - Notifications
+"""
+
+# destroy types
+DESTROY_REV = u"DESTROY_REV"
+DESTROY_ALL = u"DESTROY_ALL"
