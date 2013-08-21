4904ce23c48ea985578d151b00cb4e32c4749ce1
sharky93 <rishabhr123@gmail.com>
2013-08-21 23:54:00 +0530

    adds alert before unload on changing textareas (editor)

--- MoinMoin/templates/basic.js
+++ MoinMoin/templates/basic.js
@@ -1,5 +1,15 @@
 $(document).ready(function (){
     // this depends on the id's used for different tab-panes in modify.html (Basic Theme)
+    var edit = false;
     $('#meta, #help').removeClass('active');
     $('textarea').autosize();
+    $('textarea').change(function() {
+    	edit = true;
+    })
+    window.onbeforeunload = function(e) {
+    	if (edit) {
+    		edit = false;
+    		return "You have unsaved changes!";
+    	}
+    }
 });
