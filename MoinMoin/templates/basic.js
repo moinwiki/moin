d57d7b6ff1884faa400877b39d35eb1ac8266dd3
sharky93 <rishabhr123@gmail.com>
2013-08-27 22:16:59 +0530

    initial version of user settings view with Bootstrap

--- MoinMoin/templates/basic.js
+++ MoinMoin/templates/basic.js
@@ -2,6 +2,7 @@ $(document).ready(function (){
     // this depends on the id's used for different tab-panes in modify.html (Basic Theme)
     var edit = false;
     $('#meta, #help').removeClass('active');
+    $('#password, #notification, #ui, #navigation, #options').removeClass('active');
     $('textarea').autosize();
     window.onbeforeunload = function(e) {
         // checks if the URL is of the form http://host/+modify/page
