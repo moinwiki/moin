cbd73fe31d1a9d30368b9e2b95ffc865fc954359
Ana Balica <ana.balica@gmail.com>
2013-09-03 22:59:38 +0300

    change subscriptions funcs definitions

--- MoinMoin/util/subscriptions.py
+++ MoinMoin/util/subscriptions.py
@@ -23,25 +23,32 @@ logging = log.getLogger(__name__)
 Subscriber = namedtuple('Subscriber', [ITEMID, NAME, EMAIL, LOCALE])
 
 
-def get_subscribers(item):
+def get_subscribers(**meta):
     """ Get all users that are subscribed to the item
 
-    :param item: Item object
+    :param meta: key/value pairs from item metadata - itemid, name, namespace, tags keys
     :return: a set of Subscriber objects
     """
-    meta = item.meta
-    namespace = meta[NAMESPACE]
-    terms = [Term(SUBSCRIPTION_IDS, "{0}:{1}".format(ITEMID, meta[ITEMID])), ]
-    terms.extend(Term(SUBSCRIPTION_IDS, "{0}:{1}:{2}".format(NAME, namespace, name))
-                 for name in meta[NAME])
-    terms.extend(Term(SUBSCRIPTION_IDS, "{0}:{1}:{2}".format(TAGS, namespace, tag))
-                 for tag in meta[TAGS])
+    itemid = meta.get(ITEMID)
+    name = meta.get(NAME)
+    namespace = meta.get(NAMESPACE)
+    tags = meta.get(TAGS)
+    terms = []
+    if itemid is not None:
+        terms.extend([Term(SUBSCRIPTION_IDS, "{0}:{1}".format(ITEMID, itemid))])
+    if namespace is not None:
+        if name is not None:
+            terms.extend(Term(SUBSCRIPTION_IDS, "{0}:{1}:{2}".format(NAME, namespace, name_))
+                         for name_ in name)
+        if tags is not None:
+            terms.extend(Term(SUBSCRIPTION_IDS, "{0}:{1}:{2}".format(TAGS, namespace, tag))
+                         for tag in tags)
     query = Or(terms)
     # TODO: check ACL behaviour - is this avoiding the protection layer?
     with flaskg.storage.indexer.ix[LATEST_REVS].searcher() as searcher:
         result_iterators = [searcher.search(query, limit=None), ]
         subscription_patterns = searcher.lexicon(SUBSCRIPTION_PATTERNS)
-        patterns = get_matched_subscription_patterns(item, subscription_patterns)
+        patterns = get_matched_subscription_patterns(subscription_patterns, **meta)
         result_iterators.extend(searcher.documents(subscription_patterns=pattern) for pattern in patterns)
         subscribers = set()
         for user in chain.from_iterable(result_iterators):
@@ -52,24 +59,24 @@ def get_subscribers(item):
     return subscribers
 
 
-def get_matched_subscription_patterns(item, subscription_patterns):
+def get_matched_subscription_patterns(subscription_patterns, **meta):
     """ Get all the subscriptions with patterns that match at least one of item names
 
-    :param item: Item object
     :param subscription_patterns: a list of subscription patterns (the ones that
                                     start with NAMERE or NAMEPREFIX)
+    :param meta: key/value pairs from item metadata - name and namespace keys
     :return: a list of matched subscription patterns
     """
-    meta = item.meta
-    item_namespace = meta[NAMESPACE]
+    item_names = meta.get(NAME)
+    item_namespace = meta.get(NAMESPACE)
     matched_subscriptions = []
     for subscription in subscription_patterns:
         keyword, value = subscription.split(":", 1)
-        if keyword in (NAMEPREFIX, NAMERE, ):
+        if keyword in (NAMEPREFIX, NAMERE, ) and item_namespace is not None and item_names:
             namespace, pattern = value.split(":", 1)
             if item_namespace == namespace:
                 if keyword == NAMEPREFIX:
-                    if any(name.startswith(pattern) for name in meta[NAME]):
+                    if any(name.startswith(pattern) for name in item_names):
                         matched_subscriptions.append(subscription)
                 elif keyword == NAMERE:
                     try:
@@ -77,6 +84,6 @@ def get_matched_subscription_patterns(item, subscription_patterns):
                     except re.error:
                         logging.error("Subscription pattern '{0}' has failed compilation.".format(pattern))
                         continue
-                    if any(pattern.search(name) for name in meta[NAME]):
+                    if any(pattern.search(name) for name in item_names):
                         matched_subscriptions.append(subscription)
     return matched_subscriptions
