b8f55cd05aa6ed6b98d33e31616a797021e327bb
sharky93 <rishabhr123@gmail.com>
2013-09-08 22:01:34 +0530

    moves Item Navigation to the left, SiteMap and ItemIndex to the tabs

--- MoinMoin/themes/__init__.py
+++ MoinMoin/themes/__init__.py
@@ -82,6 +82,7 @@ class ThemeSupport(object):
     def get_action_tabs(self, item_name, current_endpoint):
         navtabs_endpoints = ['frontend.show_item', 'frontend.history',
                             'frontend.show_item_meta', 'frontend.highlight_item', 'frontend.backrefs',
+                            'frontend.index', 'frontend.sitemap',
                             ]
         if self.user.may.write(item_name):
             navtabs_endpoints.append('frontend.modify_item')
@@ -115,7 +116,7 @@ class ThemeSupport(object):
 
     def get_local_panel(self, item_name):
         user_actions_endpoints = ['frontend.quicklink_item', 'frontend.subscribe_item', ]
-        item_navigation_endpoints = ['frontend.index', 'frontend.sitemap', 'special.supplementation']
+        item_navigation_endpoints = ['special.supplementation']
         item_actions_endpoints = ['frontend.rename_item', 'frontend.delete_item', 'frontend.destroy_item',
                 'frontend.similar_names', 'frontend.download_item', 
                 'frontend.copy_item',] if self.user.may.write(item_name) else []
