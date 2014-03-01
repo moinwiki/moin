5751a00aa2faa21f3cc68b27016b39c51bc3c245
RogerHaase <haaserd@gmail.com>
2014-03-01 10:26:37 -0700

    fix #404 UndefinedError: 'fqname' is undefined - when acessing User Settings from foobar theme

---
+++ MoinMoin/auth/_tests/test_auth.py
@@ -0,0 +1,85 @@
+# Copyright: 2011 Prashant Kumar <contactprashantat AT gmail DOT com>
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+Test for auth.__init__
+"""
+
+from flask import g as flaskg
+
+from MoinMoin._tests import wikiconfig
+from MoinMoin.constants.misc import ANON
+from MoinMoin.auth import GivenAuth, handle_login, get_multistage_continuation_url
+from MoinMoin.user import create_user
+
+
+class TestConfiguredGivenAuth(object):
+    """ Test: configured GivenAuth """
+    class Config(wikiconfig.Config):
+        auth = [GivenAuth(user_name=u'JoeDoe', autocreate=True), ]
+
+    def test(self):
+        assert flaskg.user.name == [u'JoeDoe', ]
+
+
+class TestGivenAuth(object):
+    """ Test: GivenAuth """
+    def test_decode_username(self):
+        givenauth_obj = GivenAuth()
+        result1 = givenauth_obj.decode_username('test_name')
+        assert result1 == u'test_name'
+        result2 = givenauth_obj.decode_username(123.45)
+        assert result2 == 123.45
+
+    def test_transform_username(self):
+        givenauth_obj = GivenAuth()
+        givenauth_obj.strip_maildomain = True
+        givenauth_obj.strip_windomain = True
+        givenauth_obj.titlecase = True
+        givenauth_obj.remove_blanks = True
+        result = givenauth_obj.transform_username(u'testDomain\\test name@moinmoin.org')
+        assert result == 'TestName'
+
+    def test_request(self):
+        givenauth_obj = GivenAuth()
+        flaskg.user.auth_method = 'given'
+        givenauth_obj.user_name = u'testDomain\\test_user@moinmoin.org'
+        givenauth_obj.strip_maildomain = True
+        givenauth_obj.strip_windomain = True
+        givenauth_obj.titlecase = True
+        givenauth_obj.remove_blanks = True
+        create_user(u'Test_User', u'test_pass', u'test@moinmoin.org')
+        test_user, bool_value = givenauth_obj.request(flaskg.user)
+        assert test_user.valid
+        assert test_user.name == [u'Test_User', ]
+
+
+def test_handle_login():
+    # no messages in the beginning
+    assert not flaskg._login_messages
+    test_user1 = handle_login(flaskg.user, login_username='test_user', login_password='test_password', stage='moin')
+    test_login_message = [u'Invalid username or password.']
+    assert flaskg._login_messages == test_login_message
+    assert test_user1.name0 == ANON
+    assert not test_user1.valid
+    # pop the message
+    flaskg._login_messages.pop()
+    # try with a valid user
+    givenauth_obj = GivenAuth()
+    flaskg.user.auth_method = 'given'
+    givenauth_obj.user_name = u'Test_User'
+    create_user(u'Test_User', u'test_pass', u'test@moinmoin.org')
+    test_user, bool_value = givenauth_obj.request(flaskg.user)
+    test_user2 = handle_login(test_user, login_username='Test_User', login_password='test_pass', stage='moin')
+    assert not flaskg._login_messages
+    assert test_user2.name == [u'Test_User', ]
+    assert test_user2.valid
+
+
+def test_get_multistage_continuation_url():
+    test_url = get_multistage_continuation_url('test_auth_name',
+                                               extra_fields={'password': 'test_pass', 'test_key': 'test_value'})
+    assert 'test_key=test_value' in test_url
+    assert 'password=test_pass' in test_url
+    assert 'stage=test_auth_name' in test_url
+    assert 'login_submit=1' in test_url
---
+++ MoinMoin/util/_tests/test_subscriptions.py
@@ -0,0 +1,133 @@
+# Copyright: 2013 MoinMoin:AnaBalica
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+    MoinMoin - MoinMoin.util.subscriptions Tests
+"""
+
+import pytest
+
+from MoinMoin import user
+from MoinMoin.items import Item
+from MoinMoin.constants.keys import (ACL, ITEMID, CONTENTTYPE, NAME, NAMERE, NAMEPREFIX,
+                                     SUBSCRIPTIONS, TAGS)
+from MoinMoin.constants.namespaces import NAMESPACE_DEFAULT, NAMESPACE_USERPROFILES
+from MoinMoin.util.subscriptions import get_subscribers, get_matched_subscription_patterns
+
+
+class TestSubscriptions(object):
+    reinit_storage = True
+
+    def setup_method(self, method):
+        # create an item
+        self.item_name = u'foo'
+        self.tagname = u'XXX'
+        self.namespace = NAMESPACE_DEFAULT
+        meta = {CONTENTTYPE: u'text/plain;charset=utf-8', TAGS: [self.tagname]}
+        item = Item.create(self.item_name)
+        item._save(meta)
+        self.item = Item.create(self.item_name)
+
+    def test_get_subscribers(self):
+        users = get_subscribers(**self.item.meta)
+        assert users == set()
+
+        name1 = u'baz'
+        password = u'password'
+        email1 = u'baz@example.org'
+        name2 = u"bar"
+        email2 = u"bar@example.org"
+        name3 = u"barbaz"
+        email3 = u"barbaz@example.org"
+        user.create_user(username=name1, password=password, email=email1, validate=False, locale=u'en')
+        user1 = user.User(name=name1, password=password)
+        user.create_user(username=name2, password=password, email=email2, validate=False)
+        user2 = user.User(name=name2, password=password)
+        user.create_user(username=name3, password=password, email=email3, locale=u"en")
+        user3 = user.User(name=name3, password=password, email1=email3)
+        subscribers = get_subscribers(**self.item.meta)
+        assert subscribers == set()
+
+        namere = r'.*'
+        nameprefix = u"fo"
+        subscription_lists = [
+            ["{0}:{1}".format(ITEMID, self.item.meta[ITEMID])],
+            ["{0}:{1}:{2}".format(TAGS, self.namespace, self.tagname)],
+            ["{0}:{1}:{2}".format(NAME, self.namespace, self.item_name)],
+            ["{0}:{1}:{2}".format(NAMERE, self.namespace, namere)],
+            ["{0}:{1}:{2}".format(NAMEPREFIX, self.namespace, nameprefix)],
+        ]
+        users = [user1, user2, user3]
+        expected_names = {user1.name0, user2.name0}
+        for subscriptions in subscription_lists:
+            for user_ in users:
+                user_.profile._meta[SUBSCRIPTIONS] = subscriptions
+                user_.save(force=True)
+            subscribers = get_subscribers(**self.item.meta)
+            subscribers_names = {subscriber.name for subscriber in subscribers}
+            assert subscribers_names == expected_names
+
+        meta = {CONTENTTYPE: u'text/plain;charset=utf-8',
+                ACL: u"{0}: All:read,write".format(user1.name0)}
+        self.item._save(meta, comment=u"")
+        self.item = Item.create(self.item_name)
+        subscribers = get_subscribers(**self.item.meta)
+        assert {subscriber.name for subscriber in subscribers} == {user2.name0}
+
+    def test_get_matched_subscription_patterns(self):
+        meta = self.item.meta
+        patterns = get_matched_subscription_patterns([], **meta)
+        assert patterns == []
+        non_matching_patterns = [
+            "{0}:{1}:{2}".format(NAMERE, NAMESPACE_USERPROFILES, ".*"),
+            "{0}:{1}:{2}".format(NAMERE, self.namespace, "\d+"),
+            "{0}:{1}:{2}".format(NAMEPREFIX, self.namespace, "bar"),
+        ]
+        patterns = get_matched_subscription_patterns(non_matching_patterns, **meta)
+        assert patterns == []
+
+        matching_patterns = [
+            "{0}:{1}:{2}".format(NAMERE, self.namespace, "fo+"),
+            "{0}:{1}:{2}".format(NAMEPREFIX, self.namespace, "fo"),
+        ]
+        patterns = get_matched_subscription_patterns(non_matching_patterns + matching_patterns, **meta)
+        assert patterns == matching_patterns
+
+    def test_perf_get_subscribers(self):
+        pytest.skip("usually we do no performance tests")
+        password = u"password"
+        subscriptions = [
+            "{0}:{1}".format(ITEMID, self.item.meta[ITEMID]),
+            "{0}:{1}:{2}".format(NAME, self.namespace, self.item_name),
+            "{0}:{1}:{2}".format(TAGS, self.namespace, self.tagname),
+            "{0}:{1}:{2}".format(NAMEPREFIX, self.namespace, u"fo"),
+            "{0}:{1}:{2}".format(NAMERE, self.namespace, r"\wo")
+        ]
+        users = set()
+        expected_names = set()
+        for i in xrange(10000):
+            i = unicode(i)
+            user.create_user(username=i, password=password, email="{0}@example.org".format(i),
+                             validate=False, locale=u'en')
+            user_ = user.User(name=i, password=password)
+            users.add(user_)
+            expected_names.add(user_.name0)
+
+        users_sliced = list(users)[:100]
+        expected_names_sliced = {user_.name0 for user_ in users_sliced}
+        tests = [(users_sliced, expected_names_sliced), (users, expected_names)]
+
+        import time
+        for users_, expected_names_ in tests:
+            print "\nTesting {0} subscribers from a total of {1} users".format(
+                len(users_), len(users))
+            for subscription in subscriptions:
+                for user_ in users_:
+                    user_.profile._meta[SUBSCRIPTIONS] = [subscription]
+                    user_.save(force=True)
+                t = time.time()
+                subscribers = get_subscribers(**self.item.meta)
+                elapsed_time = time.time() - t
+                print "{0}: {1} s".format(subscription.split(':', 1)[0], elapsed_time)
+                subscribers_names = {subscriber.name for subscriber in subscribers}
+                assert subscribers_names == expected_names_
