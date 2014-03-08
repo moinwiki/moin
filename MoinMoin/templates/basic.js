1dbb1b2b187d600c77c22ae1588d319c8921a8cd
Saurabh Kathpalia <saurabh.kathpalia95@gmail.com>
2014-03-08 22:37:55 +0530

    Fix to issue #408

--- MoinMoin/templates/basic.js
+++ MoinMoin/templates/basic.js
@@ -2,7 +2,7 @@ $(document).ready(function (){
     // this depends on the id's used for different tab-panes in modify.html (Basic Theme)
     var edit = false;
     $('#meta, #help').removeClass('active');
-    $('#password, #notification, #ui, #navigation, #options, #acl').removeClass('active');
+    $('#password, #notification, #ui, #navigation, #options, #acl, #subscriptions').removeClass('active');
     $('textarea').autosize();
     $('#moin-save-text-button').click(function (){
         edit = true;
