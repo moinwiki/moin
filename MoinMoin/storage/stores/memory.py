5abe4fc3ec6c530697a1d481ca07783491f9ac6b
Ajitesh Gupta <ajgupta93@gmail.com>
2014-05-27 14:14:51 +0530

    Removed multiple occurences of the word 'Settings' in the 'User Settings' menu, as when translated, the translated word was becoming too long and was causing the settings menu bar to extend and become multiline. Also changed 'Wiki Appearance' to 'Appearance'.

---
+++ MoinMoin/storage/stores/memory.py
@@ -0,0 +1,58 @@
+# Copyright: 2011 MoinMoin:ThomasWaldmann
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+MoinMoin - memory store
+
+Stores k/v pairs into memory (RAM, non-persistent!).
+
+Note: likely this is mostly useful for unit tests.
+"""
+
+
+from __future__ import absolute_import, division
+
+from . import (BytesMutableStoreBase, FileMutableStoreBase,
+               BytesMutableStoreMixin, FileMutableStoreMixin)
+
+
+class BytesStore(BytesMutableStoreBase):
+    """
+    A simple dict-based in-memory store. No persistence!
+    """
+    @classmethod
+    def from_uri(cls, uri):
+        return cls()
+
+    def __init__(self):
+        self._st = None
+        self.__st = None
+
+    def create(self):
+        self.__st = {}
+
+    def destroy(self):
+        self.__st = None
+
+    def open(self):
+        self._st = self.__st
+
+    def close(self):
+        self._st = None
+
+    def __iter__(self):
+        for key in self._st:
+            yield key
+
+    def __delitem__(self, key):
+        del self._st[key]
+
+    def __getitem__(self, key):
+        return self._st[key]
+
+    def __setitem__(self, key, value):
+        self._st[key] = value
+
+
+class FileStore(FileMutableStoreMixin, BytesStore, FileMutableStoreBase):
+    """memory FileStore"""
---
+++ docs/devel/api/MoinMoin.script.rst
@@ -0,0 +1,20 @@
+MoinMoin.script package
+=======================
+
+Subpackages
+-----------
+
+.. toctree::
+
+    MoinMoin.script.account
+    MoinMoin.script.maint
+    MoinMoin.script.migration
+    MoinMoin.script.win
+
+Module contents
+---------------
+
+.. automodule:: MoinMoin.script
+    :members:
+    :undoc-members:
+    :show-inheritance:
