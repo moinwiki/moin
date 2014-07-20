3fe45323922e7af1c2a99b80f073d3d8ba34cfa6
RogerHaase <haaserd@gmail.com>
2014-07-20 15:10:31 -0700

    merge

--- MoinMoin/apps/admin/views.py
+++ MoinMoin/apps/admin/views.py
@@ -20,11 +20,13 @@ from MoinMoin.i18n import _, L_, N_
 from MoinMoin.themes import render_template, get_editor_info
 from MoinMoin.apps.admin import admin
 from MoinMoin import user
-from MoinMoin.constants.keys import NAME, ITEMID, SIZE, EMAIL, DISABLED, NAME_EXACT, WIKINAME, TRASH, NAMESPACE, NAME_OLD, REVID, MTIME, COMMENT
+from MoinMoin.constants.keys import NAME, ITEMID, SIZE, EMAIL, DISABLED, NAME_EXACT, WIKINAME, TRASH, NAMESPACE, NAME_OLD, REVID, MTIME, COMMENT, LATEST_REVS, EMAIL_UNVALIDATED, ACL
 from MoinMoin.constants.namespaces import NAMESPACE_USERPROFILES, NAMESPACE_DEFAULT, NAMESPACE_ALL
-from MoinMoin.constants.rights import SUPERUSER
-from MoinMoin.security import require_permission
+from MoinMoin.constants.rights import SUPERUSER, ACL_RIGHTS_CONTENTS
+from MoinMoin.security import require_permission, ACLStringIterator
 from MoinMoin.util.interwiki import CompositeName
+from MoinMoin.datastruct.backends.wiki_groups import WikiGroup
+from MoinMoin.datastruct.backends import GroupDoesNotExistError
 
 
 @admin.route('/superuser')
@@ -46,13 +48,21 @@ def userbrowser():
     """
     groups = flaskg.groups
     revs = user.search_users()  # all users
-    user_accounts = [dict(uid=rev.meta[ITEMID],
-                          name=rev.meta[NAME],
-                          fqname=CompositeName(NAMESPACE_USERPROFILES, NAME_EXACT, rev.name),
-                          email=rev.meta[EMAIL],
-                          disabled=rev.meta[DISABLED],
-                          groups=[groupname for groupname in groups if rev.meta[NAME] in groups[groupname]],
-                     ) for rev in revs]
+    user_accounts = []
+    for rev in revs:
+        user_groups = []
+        user_names = rev.meta[NAME]
+        for groupname in groups:
+            group = groups[groupname]
+            for name in user_names:
+                if name in group:
+                    user_groups.append(groupname)
+        user_accounts.append(dict(uid=rev.meta[ITEMID],
+                                  name=user_names,
+                                  fqname=CompositeName(NAMESPACE_USERPROFILES, NAME_EXACT, rev.name),
+                                  email=rev.meta[EMAIL] if EMAIL in rev.meta else rev.meta[EMAIL_UNVALIDATED],
+                                  disabled=rev.meta[DISABLED],
+                                  groups=user_groups))
     return render_template('admin/userbrowser.html', user_accounts=user_accounts, title_name=_(u"Users"))
 
 
@@ -249,3 +259,110 @@ def _trashed(namespace):
         meta = rev.meta
         results.append(trashedEntry(rev.fqname, meta[NAME_OLD], meta[REVID], meta[MTIME], meta[COMMENT], get_editor_info(meta)))
     return results
+
+
+@admin.route('/user_acl_report/<uid>', methods=['GET'])
+@require_permission(SUPERUSER)
+def user_acl_report(uid):
+    all_items = flaskg.storage.documents(wikiname=app.cfg.interwikiname)
+    groups = flaskg.groups
+    theuser = user.User(uid=uid)
+    itemwise_acl = []
+    for item in all_items:
+        fqname = CompositeName(item.meta.get(NAMESPACE), u'itemid', item.meta.get(ITEMID))
+        itemwise_acl.append({'name': item.meta.get(NAME),
+                             'itemid': item.meta.get(ITEMID),
+                             'fqname': fqname,
+                             'read': theuser.may.read(fqname),
+                             'write': theuser.may.write(fqname),
+                             'create': theuser.may.create(fqname),
+                             'admin': theuser.may.admin(fqname),
+                             'destroy': theuser.may.destroy(fqname)})
+    return render_template('admin/user_acl_report.html',
+                           title_name=_(u'User ACL Report'),
+                           user_names=theuser.name,
+                           itemwise_acl=itemwise_acl)
+
+
+@admin.route('/groupbrowser', methods=['GET'])
+@require_permission(SUPERUSER)
+def groupbrowser():
+    """
+    Display list of all groups and their members
+    """
+    all_groups = flaskg.groups
+    groups = []
+    for group in all_groups:
+        group_type = ''
+        if isinstance(all_groups[group], WikiGroup):
+            group_type = 'WikiGroup'
+        else:
+            group_type = 'ConfigGroup'
+        groups.append(dict(name=all_groups[group].name,
+                           member_users=all_groups[group].members,
+                           member_groups=all_groups[group].member_groups,
+                           grouptype=group_type))
+    return render_template('admin/groupbrowser.html',
+                           title_name=_(u'Groups'),
+                           groups=groups)
+
+
+@admin.route('/item_acl_report', methods=['GET'])
+@require_permission(SUPERUSER)
+def item_acl_report():
+    """
+    Return a list of all items in the wiki along with the ACL Meta-data
+    """
+    all_items = flaskg.storage.documents(wikiname=app.cfg.interwikiname)
+    items_acls = []
+    for item in all_items:
+        item_namespace = item.meta.get(NAMESPACE)
+        item_id = item.meta.get(ITEMID)
+        item_name = item.meta.get(NAME)
+        item_acl = item.meta.get(ACL)
+        fqname = CompositeName(item_namespace, u'itemid', item_id)
+        if item_acl is None:
+            for namespace, acl_config in app.cfg.acl_mapping:
+                if item_namespace == namespace or item_namespace == 'userprofiles' and namespace == 'userprofiles/':
+                    item_acl = 'Default ({0})'.format(acl_config['default'])
+        items_acls.append({'name': item_name,
+                           'itemid': item_id,
+                           'fqname': fqname,
+                           'acl': item_acl})
+    return render_template('admin/item_acl_report.html',
+                           title_name=_('Item ACL Report'),
+                           items_acls=items_acls)
+
+
+def search_group(group_name):
+    groups = flaskg.groups
+    if groups[group_name]:
+            return groups[group_name]
+    else:
+        raise GroupDoesNotExistError(group_name)
+
+
+@admin.route('/group_acl_report/<group_name>')
+@require_permission(SUPERUSER)
+def group_acl_report(group_name):
+    """
+    Display a 2-column table of items and ACLs, where the ACL rule specifies any
+    WikiGroup or ConfigGroup name.
+    """
+    group = search_group(group_name)
+    all_items = flaskg.storage.documents(wikiname=app.cfg.interwikiname)
+    group_items = []
+    for item in all_items:
+        acl_iterator = ACLStringIterator(ACL_RIGHTS_CONTENTS, item.meta.get(ACL, ''))
+        for modifier, entries, rights in acl_iterator:
+            if group_name in entries:
+                item_id = item.meta.get(ITEMID)
+                fqname = CompositeName(item.meta.get(NAMESPACE), u'itemid', item_id)
+                group_items.append(dict(name=item.meta.get(NAME),
+                                        itemid=item_id,
+                                        fqname=fqname,
+                                        rights=rights))
+    return render_template('admin/group_acl_report.html',
+                           title_name=_(u'Group ACL Report'),
+                           group_items=group_items,
+                           group_name=group_name)
