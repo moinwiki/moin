9b127079780ca3c6462ef0f13b44e1c39708b1f0
RogerHaase <haaserd@gmail.com>
2014-09-12 06:57:49 -0700

    make the appearance tab the default user settings pane

--- MoinMoin/templates/basic.js
+++ MoinMoin/templates/basic.js
@@ -2,7 +2,7 @@ $(document).ready(function (){
     // this depends on the id's used for different tab-panes in modify.html (Basic Theme)
     var edit = false;
     $('#meta, #help').removeClass('active');
-    $('#password, #notification, #ui, #navigation, #options, #acl, #subscriptions').removeClass('active');
+    $('#password, #notification, #personal, #navigation, #options, #acl, #subscriptions').removeClass('active');
     $('textarea').autosize();
     $('div.dropup').removeClass('menu');
     $('ul.dropdown-menu').removeClass('submenu');
