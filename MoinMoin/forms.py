32177ee311613064ef0015a42ec65016f76ae733
RogerHaase <haaserd@gmail.com>
2015-02-02 12:31:55 -0700

    fix #449 Traceback after clicking Save button on User Settings Personal Tab

--- MoinMoin/forms.py
+++ MoinMoin/forms.py
@@ -121,8 +121,10 @@ class ValidName(Validator):
     invalid_name_msg = ""
 
     def validate(self, element, state):
-        # Make sure that the other meta is valid before validating the name.
-        # TODO Change/Make sure that the below statement holds good.
+        if state is None:
+            # incoming request is from +usersettings#personal; apps/frontend/views.py will validate changes to user names
+            return True
+        # Make sure that the other meta is valid before validating the name.  TODO: prove the try below is always redundant and remove it.
         try:
             if not element.parent.parent['extra_meta_text'].valid:
                 return False
