138270a9bedfeeb4c4071320bc9533e1fd37f6f3
RogerHaase <haaserd@gmail.com>
2015-02-05 12:26:10 -0700

    fix #413 Traceback after clicking Save button on User Settings: Subscriptions tab

--- MoinMoin/forms.py
+++ MoinMoin/forms.py
@@ -241,7 +241,8 @@ class SubscriptionsJoinedString(JoinedString):
         subscriptions = []
         for child in self:
             if child.u.startswith(ITEMID):
-                value = re.sub(r"\(.*\)", "", child.u)
+                # itemid:67155f195938426d82502540493e8acf (creole)
+                value = child.u.split(" ", 1)[0]
                 item = flaskg.storage.document(**{ITEMID: value.split(":")[1]})
                 try:
                     name_ = item.meta['name'][0]
@@ -249,6 +250,7 @@ class SubscriptionsJoinedString(JoinedString):
                     name_ = "This item doesn't exist"
                 value = u"{0} ({1})".format(value, name_)
             else:
+                # name::ExampleItem | tags::demo | nameprefix::jp | namere::.* | name:MyNamespace:ExampleItem
                 value = child.u
             subscriptions.append(value)
         return self.separator.join(subscriptions)
