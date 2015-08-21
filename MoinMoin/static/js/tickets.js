12f11ecbedd2474ede7714d1eb759926b586c8d0
Vipul Sharma <Vipul Sharma@localhost>
2015-08-21 15:08:53 +0530

    added threaded comment feature in ticket modify view

--- MoinMoin/static/js/tickets.js
+++ MoinMoin/static/js/tickets.js
@@ -18,7 +18,44 @@ $(document).ready(function () {
     // ticket tags are initially hidden by css; if a tag is selected: show the tags
     if (location.search.indexOf("selected_tags") >= 0) {
         $('.ticket-tags-toggle').click();
+    }  
+    $('.reply').click(function(e){
+        e.preventDefault();
+        var reply_to = $(this).attr('reply_to');
+        var refers_to = $(this).attr('refers_to');
+        if( !$('#'+reply_to).find("textarea.comment-reply").length ) {
+            $('#'+reply_to).append('<div class="comment-box"><textarea class="comment-reply" type="text"/> \
+                </p><button id="save">Save</button><button id="cancel">Cancel</button></p></div>');
+            $('#'+reply_to).find("textarea.comment-reply").focus();
+            $('#save').on('click', function(e){
+                var data = $('textarea.comment-reply').val();
+                $('div.comment-box').remove();
+                post_comment(reply_to, refers_to, data);
+                return false;
+            });
+            $('#cancel').on('click', function(e) {
+                $('div.comment-box').remove();
+                return false;
+            });
+        }
+    });
+
+    function post_comment(reply_to, refers_to, data) {
+        $.ajax({
+            type: "POST",
+            url: "/+comment",
+            data: { reply_to: reply_to, refers_to: refers_to, data: data }
+        }).done(function(html) {
+            location.reload(true);
+        });
     }
+
+    $(".jumper").on("click", function( e ) {
+        e.preventDefault();
+        $("body, html").animate({
+            scrollTop: $( $(this).attr('href') ).offset().top
+            }, 600);
+    });
     // when user clicks the tag, it is added to input field
     $('.moin-ticket-tags a').click(function() {
         var value = $(this).text();
