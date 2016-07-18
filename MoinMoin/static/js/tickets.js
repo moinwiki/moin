ab5a195a2bf3c99353007679b44fefa017b81ec5
RogerHaase <haaserd@gmail.com>
2016-07-18 08:00:53 -0700

    apply jslint recommendations to ticket.js

--- MoinMoin/static/js/tickets.js
+++ MoinMoin/static/js/tickets.js
@@ -1,5 +1,18 @@
+/*global $, _*/
+
 $(document).ready(function () {
     "use strict";
+
+    function post_comment(reply_to, refers_to, data) {
+        $.ajax({
+            type: "POST",
+            url: "/+comment",
+            data: {reply_to: reply_to, refers_to: refers_to, data: data}
+        }).done(function (html) {
+            location.reload(true);
+        });
+    }
+
     $('.moin-loginsettings').addClass('navbar-right');
 
     // executed when user clicks tickets tab tags button and conditionally on page load
@@ -19,51 +32,42 @@ $(document).ready(function () {
     if (location.search.indexOf("selected_tags") >= 0) {
         $('.ticket-tags-toggle').click();
     }
-    $('.reply').click(function(e){
+
+    $('.reply').click(function (e) {
         e.preventDefault();
         var reply_to = $(this).attr('reply_to');
         var refers_to = $(this).attr('refers_to');
-        $('[comment_id='+reply_to+']').css('height', '170px');
-        if( !$('#'+reply_to).find("textarea.comment-reply").length ) {
-            $('#'+reply_to).append('<div class="comment-box"><textarea class="comment-reply" type="text"/> \
-                </p><button id="save">Save</button><button id="cancel">Cancel</button></p></div>');
-            $('#'+reply_to).find("textarea.comment-reply").focus();
-            $('#save').on('click', function(e){
+        $('[comment_id=' + reply_to + ']').css('height', '170px');
+        if (!$('#' + reply_to).find("textarea.comment-reply").length) {
+            $('#' + reply_to).append('<div class="comment-box"><textarea class="comment-reply" type="text"/></p><button id="save">Save</button><button id="cancel">Cancel</button></p></div>');
+            $('#' + reply_to).find("textarea.comment-reply").focus();
+            $('#save').on('click', function (e) {
                 var data = $('textarea.comment-reply').val();
                 $('div.comment-box').remove();
                 post_comment(reply_to, refers_to, data);
                 return false;
             });
-            $('#cancel').on('click', function(e) {
+            $('#cancel').on('click', function (e) {
                 $('div.comment-box').remove();
-                $('[comment_id='+reply_to+']').css('height', '70px');
+                $('[comment_id=' + reply_to + ']').css('height', '70px');
                 return false;
             });
         }
     });
 
-    function post_comment(reply_to, refers_to, data) {
-        $.ajax({
-            type: "POST",
-            url: "/+comment",
-            data: { reply_to: reply_to, refers_to: refers_to, data: data }
-        }).done(function(html) {
-            location.reload(true);
-        });
-    }
-
-    $(".jumper").on("click", function( e ) {
+    $(".jumper").on("click", function (e) {
         e.preventDefault();
         $("body, html").animate({
-            scrollTop: $( $(this).attr('href') ).offset().top
-            }, 600);
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 600);
     });
+
     // when user clicks the tag, it is added to input field
-    $('.moin-ticket-tags a').click(function() {
+    $('.moin-ticket-tags a').click(function () {
         var value = $(this).text();
         var input = $('#f_meta_tags');
-        if( !input.val() ) {
-            input.val(value)
+        if (!input.val()) {
+            input.val(value);
         } else {
             input.val(input.val() + ', ' + value);
         }
