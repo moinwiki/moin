7a13224a01e0d1b09581af8b0c81eaedf5e225fc
RogerHaase <RogerHaase@localhost>
2016-08-09 12:23:52 -0700

    rework item history, show and meta views adding revision navigation, fixes #513 #555

---
+++ MoinMoin/util/rev_navigation.py
@@ -0,0 +1,36 @@
+# Copyright: 2016 MoinMoin:RogerHaase
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+    Helper function to get prior, current, next revisions and mod-times.
+"""
+
+from flask import g as flaskg
+from flask import current_app as app
+from whoosh.query import Term, And
+from MoinMoin.constants.keys import *
+
+
+def prior_next_revs(revid, fqname):
+    """
+    Return prior, current, and next revids and time stamps.
+    """
+    terms = [Term(WIKINAME, app.cfg.interwikiname), ]
+    terms.extend(Term(term, value) for term, value in fqname.query.iteritems())
+    query = And(terms)
+    revs = flaskg.storage.search(query, idx_name=ALL_REVS, sortedby=[MTIME], reverse=True, limit=None)
+    rev_ids = []
+    mtimes = []
+    for rev in revs:
+        mtimes.append(dict(rev.meta)[u'mtime'])
+        rev_ids.append(rev.revid)
+    prior_rev = next_rev = prior_mtime = next_mtime = None
+    current_idx = rev_ids.index(revid)
+    current_mtime = mtimes[current_idx]
+    if current_idx:
+        next_rev = rev_ids[current_idx - 1]
+        next_mtime = mtimes[current_idx - 1]
+    if current_idx < len(rev_ids) - 1:
+        prior_rev = rev_ids[current_idx + 1]
+        prior_mtime = mtimes[current_idx + 1]
+    return (prior_rev, revid, next_rev, prior_mtime, current_mtime, next_mtime)
