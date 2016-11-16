0e0670fd462bdae4de221e55ad6d16acd4ce9bae
RogerHaase <haaserd@gmail.com>
2016-11-16 16:14:11 -0700

    add HighlighterList macro to display list of Pygments lexers

---
+++ MoinMoin/macro/HighlighterList.py
@@ -0,0 +1,28 @@
+# Copyright:  2016 MoinMoin:RogerHaase
+# License: GNU GPL v2 (or any later version), see LICENSE.txt for details.
+
+"""
+HighlighterList - display a list of Pygments lexers
+
+Usage: <<HighlighterList>>
+"""
+
+import pygments
+
+from MoinMoin.macro._base import MacroBlockBase
+from MoinMoin.i18n import _, L_, N_
+from MoinMoin.converter._table import TableMixin
+
+
+class Macro(MacroBlockBase):
+    def macro(self, content, arguments, page_url, alternative):
+        headings = (_('Lexer Name'),
+                    _('Lexer Aliases'),
+                    _('File Patterns'),
+                    _('Mimetypes'),
+                   )
+        rows = list(pygments.lexers.get_all_lexers())
+        rows.sort(key=lambda t: tuple(t[0].lower()))
+        table = TableMixin()
+        ret = table.build_dom_table(rows, head=headings, cls='moin-sortable')
+        return ret
