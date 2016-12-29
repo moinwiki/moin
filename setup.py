b9314a75c0ba9b51c93f262a3ec74f2da572e072
RogerHaase <RogerHaase@localhost>
2016-12-29 13:22:20 -0700

    <<< run ./m quickinstall after hg pull >>> upgrade Flask-Themes to 0.3.0, flask.ext deprecated in Flask 0.11

--- setup.py
+++ setup.py
@@ -78,7 +78,8 @@ setup_args = dict(
         # 'https://github.com/mitsuhiko/flask/tarball/master#egg=Flask-0.11-dev',
         # 'https://bitbucket.org/thomaswaldmann/whoosh/get/default.tar.gz#egg=Whoosh-2.5.99dev',
         # use a pre-release and fixed flask-themes 0.2.0 as 0.1.3 does not work for flask 0.8.x:
-        'https://bitbucket.org/thomaswaldmann/flask-themes/get/a87399231886.tar.gz#egg=Flask-Themes-0.2.0',
+        # 'https://bitbucket.org/thomaswaldmann/flask-themes/get/a87399231886.tar.gz#egg=Flask-Themes-0.2.0',
+        'https://bitbucket.org/RogerHaase/flask-themes/get/6f0fbeb3156b.tar.gz#egg=Flask-Themes-0.3.0',
         'https://bitbucket.org/thomaswaldmann/flatland-fixes/get/bb7ecc64bf96.tar.gz#egg=flatland-0.0.3',
     ],
     install_requires=[
@@ -89,7 +90,7 @@ setup_args = dict(
         'Flask-Babel>=0.11.1',  # i18n support
         'Flask-Cache>=0.3.4',  # caching support
         'Flask-Script==1.0.0',  # scripting support; see #429, test "./m sample" after changing this
-        'Flask-Themes>=0.2.0',  # theme support
+        'Flask-Themes>=0.3.0',  # theme support
         'emeraldtree>=0.10.0',  # xml processing
         'flatland',  # form handling
         'Jinja2>=2.7',  # template engine
