3f1193036a3f38611b0fba435e0a0b913d2687db
RogerHaase <haaserd@gmail.com>
2017-04-22 13:16:09 -0700

    reword error message to reflect more possible reasons for failure when user incorrectly changes metadata

--- MoinMoin/apps/frontend/views.py
+++ MoinMoin/apps/frontend/views.py
@@ -639,9 +639,9 @@ def modify_item(item_name):
         return item.do_modify()
     except UnicodeDecodeError:
         return _crash(item, None, None)
-    except ValueError:  # strict data validation failed in indexing.py
-        flash(_("""Error: nothing changed. Data validation failed because input data did not match content-type.
-                   To change an item to a different content-type: delete the item, then recreate."""), "error")
+    except ValueError:
+        # user may have changed or deleted namespace, contenttype, or strict data validation failed in indexing.py
+        flash(_("""Error: nothing changed. Meta data validation failed."""), "error")
         return redirect(url_for_item(item_name))
 
 
