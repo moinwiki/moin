a633c38d2af8d7db9befba9709564304b68f6011
Thomas Waldmann <tw@waldmann-edv.de>
2019-07-13 17:49:15 +0200

    py3: adapt setup.py / pypi metadata

--- src/moin/__init__.py
+++ src/moin/__init__.py
@@ -17,5 +17,5 @@ from ._version import version
 project = "MoinMoin"
 
 
-if sys.hexversion < 0x2070000 or sys.hexversion > 0x2999999:
-    sys.exit("Error: %s requires Python 2.7.x., current version is %s\n" % (project, platform.python_version()))
+if sys.hexversion < 0x3050000:
+    sys.exit("Error: %s requires Python 3.5+, current version is %s\n" % (project, platform.python_version()))
