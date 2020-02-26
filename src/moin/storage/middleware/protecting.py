8dcd78557f89c826a4eb78d5c4896a14249cb50e
RogerHaase <haaserd@gmail.com>
2020-02-26 08:09:36 -0700

    no code change, move method positions to prepare for addition of new method

--- src/moin/storage/middleware/protecting.py
+++ src/moin/storage/middleware/protecting.py
@@ -149,6 +149,12 @@ class ProtectingMiddleware:
             if rev.allows(READ) or rev.allows(PUBREAD):
                 yield rev
 
+    def search_page(self, q, idx_name=LATEST_REVS, pagenum=1, pagelen=10, **kw):
+        for rev in self.indexer.search_page(q, idx_name, pagenum, pagelen, **kw):
+            rev = ProtectedRevision(self, rev)
+            if rev.allows(READ) or rev.allows(PUBREAD):
+                yield rev
+
     def search_meta(self, q, idx_name=LATEST_REVS, **kw):
         """
         Yield an item's metadata, skipping any items where read permission is denied.
@@ -173,12 +179,6 @@ class ProtectingMiddleware:
             result = rev.allows(READ) or rev.allows(PUBREAD)
             last_rev_result = result
             if result:
-                yield rev
-
-    def search_page(self, q, idx_name=LATEST_REVS, pagenum=1, pagelen=10, **kw):
-        for rev in self.indexer.search_page(q, idx_name, pagenum, pagelen, **kw):
-            rev = ProtectedRevision(self, rev)
-            if rev.allows(READ) or rev.allows(PUBREAD):
                 yield rev
 
     def search_results_size(self, q, idx_name=ALL_REVS, **kw):
