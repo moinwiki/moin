ff52db27304195e7059db398956d50b5056a9bdd
RogerHaase <haaserd@gmail.com>
2020-04-30 12:47:27 -0700

    add comments to moinwiki_out, style attribute is not supported in span tags

--- src/moin/converters/moinwiki_out.py
+++ src/moin/converters/moinwiki_out.py
@@ -538,6 +538,9 @@ class Converter:
         return Moinwiki.separator + hr_ending
 
     def open_moinpage_span(self, elem):
+        # moin syntax does not support style attributes within span tags.
+        # Colored text or backgrounds supported by html, markdown extensions, etc
+        # are ignored and not converted.
         font_size = elem.get(moin_page.font_size, '')
         baseline_shift = elem.get(moin_page.baseline_shift, '')
         class_ = elem.get(moin_page.class_, '')
