9c9af4537fa10cde6b031b8a8de1b09c5dc7cddd
Stephane Blondon <stephane.blondon@gmail.com>
2020-06-02 19:16:41 +0200

    Jinja and Werkzeug dependancies are included in Flask

--- setup.py
+++ setup.py
@@ -86,9 +86,7 @@ setup_args = dict(
         'emeraldtree>=0.10.0',  # xml processing
         'feedgen==0.9.*',  # Atom feed
         'flatland>=0.8',  # form handling
-        'Jinja2>=2.7',  # template engine
         'pygments>=1.4',  # src code / text file highlighting
-        'Werkzeug>=1.0.0',  # wsgi toolkit
         'whoosh>=2.7.0',  # needed for indexed search
         'pdfminer3',  # pdf -> text/plain conversion
         'passlib>=1.6.0',  # strong password hashing (1.6 needed for consteq)
