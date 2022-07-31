43169188dddd503654c86ac0a623006b90829bf7
RogerHaase <haaserd@gmail.com>
2022-07-31 13:23:55 -0700

    fix pep8 errors, try 3

--- src/moin/apps/frontend/views.py
+++ src/moin/apps/frontend/views.py
@@ -1957,7 +1957,7 @@ class ValidLostPassword(Validator):
 
     def validate(self, element, state):
         if not (element['username'].valid and element['username'].value or
-               element['email'].valid and element['email'].value):
+                element['email'].valid and element['email'].value):
             return self.note_error(element, state, 'name_or_email_needed_msg')
 
         return True
