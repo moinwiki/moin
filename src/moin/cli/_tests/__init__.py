229fa337337c4b70bb41e3f7deb226f6e264c6da
bylsmad <25931535+bylsmad@users.noreply.github.com>
2023-04-17 18:22:52 -0700

    checking format before eval, fixes #1409

--- src/moin/cli/_tests/__init__.py
+++ src/moin/cli/_tests/__init__.py
@@ -8,8 +8,10 @@ MoinMoin - moin.cli common functions for tests
 from copy import copy
 import datetime  # noqa
 import os
+import re
 import subprocess
 from typing import List
+from warnings import warn
 
 from moin import log
 
@@ -33,6 +35,60 @@ def assert_p_succcess(p: subprocess.CompletedProcess):
         raise
 
 
+ARRAY_SPLIT_RE = re.compile(r'[\[\],]')
+DATETIME_RE = re.compile(r'datetime\.datetime\([\d, ]*\)')
+COUNT_SLASHES_RE = re.compile(r'([\\]*)$')
+
+
+def _is_eval_safe(s: str) -> bool:
+    """validate that s is one of the expected formats in output of moin index-dump
+
+    valid strings include
+    'a string'
+    'a string doesn\'t have to be simple'
+    ['one', 'two']
+    []
+    123
+    datetime.datetime(2023, 4, 17, 22, 52, 43)"""
+    if s.startswith('['):
+        words = ARRAY_SPLIT_RE.split(s)
+    else:
+        words = [s]
+    for word in words:
+        safe = False
+        string_delimiter = None
+        word = word.strip()
+        if not word:
+            safe = True
+        else:
+            for d in ["'", '"']:
+                if word.startswith(d):
+                    string_delimiter = d
+                    break
+            if string_delimiter:
+                safe = True
+                if not word.endswith(string_delimiter):
+                    safe = False
+                fragments = word[1:-1].split(string_delimiter)[0:-1]
+                if fragments:
+                    for fragment in fragments:
+                        m = COUNT_SLASHES_RE.search(fragment)
+                        if m.groups() and len(m.groups()[0]) % 2 == 0:
+                            safe = False
+                            break
+            else:
+                try:
+                    int(s)
+                    safe = True
+                except ValueError:
+                    pass
+                if DATETIME_RE.fullmatch(s):
+                    safe = True
+        if not safe:
+            return False
+    return True
+
+
 def read_index_dump_latest_revs(out: str):
     """parse output of moin dump-index yielding the items in latest revs
 
@@ -50,4 +106,8 @@ def read_index_dump_latest_revs(out: str):
                 break
             continue
         space_index = line.index(' ')
-        item[line[0:space_index]] = eval(line[space_index + 1:])
+        v_str = line[space_index + 1:].strip()
+        if not _is_eval_safe(v_str):
+            warn(f'invalid line in stdout of moin index-dump: {repr(line.strip())}')
+            continue
+        item[line[0:space_index]] = eval(v_str)
