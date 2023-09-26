410906552baa0dcfebe60254c8ec66ecce908b8e
RogerHaase <haaserd@gmail.com>
2023-09-26 14:42:58 -0700

    add namespace and test options to maint-reduce-revisions; fixes #1524

--- src/moin/cli/maint/reduce_revisions.py
+++ src/moin/cli/maint/reduce_revisions.py
@@ -18,7 +18,7 @@ from flask.cli import FlaskGroup
 
 from whoosh.query import Every, Term, And, Regex
 
-from moin.constants.keys import NAME, NAME_EXACT, REVID, WIKINAME, PARENTID, REV_NUMBER
+from moin.constants.keys import NAME, NAME_EXACT, NAMESPACE, REVID, WIKINAME, PARENTID, REV_NUMBER
 from moin.app import create_app, before_wiki
 
 from moin import log
@@ -31,46 +31,60 @@ def cli():
 
 
 @cli.command("maint-reduce-revisions",
-             help="Remove all revisions but the last one from all selected items")
+             help="Remove all revisions but the last one from all selected items.")
 @click.option('--query', '-q', type=str, default='',
               help='Only perform the operation on items found by the given query.')
-def ReduceRevisions(query):
+@click.option('--namespace', '-n', type=str, default='',
+              help='Limit selection to this namespace.')
+@click.option('--test', '-t', type=bool, default=0,
+              help='List selected items, but do not update.')
+def ReduceRevisions(query, namespace, test):
     logging.info("Reduce revisions started")
     before_wiki()
-    if query:
-        q = And([Term(WIKINAME, app.cfg.interwikiname), Regex(NAME_EXACT, query)])
+    q = Term(WIKINAME, app.cfg.interwikiname)
+    if query or namespace:
+        if query:
+            q = And([q, Regex(NAME_EXACT, query)])
+        if namespace:
+            q = And([q, Regex(NAMESPACE, namespace)])
     else:
         q = Every()
 
     for current_rev in app.storage.search(q, limit=None):
         current_name = current_rev.meta[NAME]
         current_revid = current_rev.meta[REVID]
-        print("Destroying historical revisions of {0!r}:".format(current_name))
-        has_historical_revision = False
-        for rev in current_rev.item.iter_revs():
-            revid = rev.meta[REVID]
-            if revid == current_revid:
-                # fixup metadata and overwrite existing revision; modified time will be updated if changed
-                changed = False
-                meta = dict(rev.meta)
-                if REV_NUMBER in meta and meta[REV_NUMBER] > 1 or REV_NUMBER not in meta:
-                    changed = True
-                    meta[REV_NUMBER] = 1
-                if PARENTID in meta:
-                    changed = True
-                    del meta[PARENTID]
-                if changed:
-                    current_rev.item.store_revision(meta, current_rev.data, overwrite=True)
-                    print("    (current rev meta data updated)")
-                continue
-            has_historical_revision = True
-            name = rev.meta[NAME]
-            if name == current_name:
-                print("    Destroying revision {0}".format(revid))
-            else:
-                print("    Destroying revision {0} (named {1!r})".format(revid, name))
-            current_rev.item.destroy_revision(revid)
-        if not has_historical_revision:
-            print("    (no historical revisions)")
+        current_namespace = current_rev.meta[NAMESPACE]
+        current_revno = current_rev.meta[REV_NUMBER]
+        current_full_name = current_namespace + '/' + current_name[0] if current_namespace else current_name
+        if test:
+            print("Item named {0!r} selected but not updated, has {1} revisions :".format(current_full_name, current_revno))
+        else:
+            print("Destroying historical revisions of {0!r}:".format(current_full_name))
+            has_historical_revision = False
+            for rev in current_rev.item.iter_revs():
+                revid = rev.meta[REVID]
+                if revid == current_revid:
+                    # fixup metadata and overwrite existing revision; modified time will be updated if changed
+                    changed = False
+                    meta = dict(rev.meta)
+                    if REV_NUMBER in meta and meta[REV_NUMBER] > 1 or REV_NUMBER not in meta:
+                        changed = True
+                        meta[REV_NUMBER] = 1
+                    if PARENTID in meta:
+                        changed = True
+                        del meta[PARENTID]
+                    if changed:
+                        current_rev.item.store_revision(meta, current_rev.data, overwrite=True)
+                        print("    (current rev meta data updated)")
+                    continue
+                has_historical_revision = True
+                name = rev.meta[NAME]
+                if name == current_name:
+                    print("    Destroying revision {0}".format(revid))
+                else:
+                    print("    Destroying revision {0} (named {1!r})".format(revid, name))
+                current_rev.item.destroy_revision(revid)
+            if not has_historical_revision:
+                print("    (no historical revisions)")
 
     logging.info("Reduce revisions finished")
