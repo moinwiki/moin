9b7a96c79ea167c5fee9bad4cbc58e242b8fac5b
Dave St.Germain <dave@st.germa.in>
2024-05-18 10:25:01 -0400

    Support for SQLAlchemy 2.0

--- src/moin/storage/backends/_tests/test_stores.py
+++ src/moin/storage/backends/_tests/test_stores.py
@@ -18,6 +18,8 @@ from moin.storage.stores.memory import BytesStore as MemoryBytesStore
 from moin.storage.stores.memory import FileStore as MemoryFileStore
 from moin.storage.stores.fs import BytesStore as FSBytesStore
 from moin.storage.stores.fs import FileStore as FSFileStore
+from moin.storage.stores.sqla import BytesStore as SQLABytesStore
+from moin.storage.stores.sqla import FileStore as SQLAFileStore
 
 
 class TestMemoryBackend(MutableBackendTestBase):
@@ -37,6 +39,17 @@ class TestFSBackend(MutableBackendTestBase):
         data_path = tempfile.mkdtemp()
         os.rmdir(data_path)
         data_store = FSFileStore(data_path)
+        self.be = MutableBackend(meta_store, data_store)
+        self.be.create()
+        self.be.open()
+
+
+class TestSQLABackend(MutableBackendTestBase):
+    def setup_method(self, method):
+        meta_path = tempfile.mktemp()
+        data_path = tempfile.mktemp()
+        meta_store = SQLABytesStore(f"sqlite:///{meta_path}")
+        data_store = SQLAFileStore(f"sqlite:///{data_path}")
         self.be = MutableBackend(meta_store, data_store)
         self.be.create()
         self.be.open()
