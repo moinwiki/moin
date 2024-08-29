6c37b7d377dd942d65982ba37a65f5671b334ab3
UlrichB22 <97119703+UlrichB22@users.noreply.github.com>
2024-08-29 21:44:00 +0200

    SlideShow: use url_for()

--- src/moin/macros/SlideShow.py
+++ src/moin/macros/SlideShow.py
@@ -5,6 +5,7 @@
 Create link to start a SlideShow for the current item
 """
 
+from flask import url_for
 from moin.i18n import _
 from moin.utils.tree import moin_page, xlink
 from moin.macros._base import MacroInlineBase
@@ -14,5 +15,6 @@ class Macro(MacroInlineBase):
     def macro(self, content, arguments, page_url, alternative):
         attrib = {moin_page.class_: "fa-regular fa-circle-play"}
         children = [moin_page.span(attrib=attrib), _(" Start SlideShow")]
-        result = moin_page.a(attrib={xlink.href: f"/+slideshow{page_url.path}"}, children=children)
+        url = url_for("frontend.slide_item", item_name=page_url.path[1:])
+        result = moin_page.a(attrib={xlink.href: url}, children=children)
         return result
