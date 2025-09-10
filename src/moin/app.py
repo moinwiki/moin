1f8c12e59bb5ace034d5eb0a48a9c189641235c1
Roland Rüdenauer <roland.ruedenauer@gmail.com>
2025-09-10 22:25:12 +0200

    Correct cspreport log path used in before_wiki (add leading slash)

--- src/moin/app.py
+++ src/moin/app.py
@@ -317,7 +317,7 @@ def before_wiki():
     """
     Setup environment for wiki requests, start timers.
     """
-    if request and (is_static_content(request.path) or request.path == "+cspreport/log"):
+    if request and (is_static_content(request.path) or request.path == "/+cspreport/log"):
         logging.debug(f"skipping before_wiki for {request.path}")
         return
 
