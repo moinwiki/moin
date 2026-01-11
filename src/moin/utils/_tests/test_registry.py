9590666a22a57ffd0ba68b6b2569c9956ae7d9ea
Roland Rüdenauer <roland.ruedenauer@gmail.com>
2026-01-11 17:34:01 +0100

    Tests: adapt moin.utils.registry

--- src/moin/utils/_tests/test_registry.py
+++ src/moin/utils/_tests/test_registry.py
@@ -5,30 +5,60 @@
 MoinMoin - moin.utils.registry tests.
 """
 
+from __future__ import annotations
+
+from typing import Any, cast, Callable, NamedTuple
 
 import pytest
 
-from moin.utils.registry import Registry
+from moin.utils.registry import RegistryBase
 
 
-def factory_all(arg):
+class Registry(RegistryBase[int]):
+
+    class Entry(NamedTuple):
+        factory: Callable[..., int | None]
+        priority: int
+
+        def __call__(self, *args: Any, **kwargs: Any) -> int | None:
+            return self.factory(*args, **kwargs)
+
+        def __lt__(self, other: Any):
+            if isinstance(other, self.__class__):
+                return self.priority < other.priority
+            return NotImplemented
+
+    def register(self, factory: Callable[..., int | None], priority: int = RegistryBase.PRIORITY_MIDDLE):
+        """
+        Register a factory
+
+        :param factory: Factory to register. Callable, have to return a class
+        """
+        self._register(self.Entry(factory, priority))
+
+    def at(self, index: int) -> Registry.Entry:
+        return cast(Registry.Entry, self._entries[index])
+
+
+def factory_all(arg: str | None) -> int:
     return 1
 
 
-def factory_all2(arg):
+def factory_all2(arg: str | None) -> int:
     return 3
 
 
-def factory_none(arg):
+def factory_none(arg: str | None) -> None:
     pass
 
 
-def factory_special(arg):
+def factory_special(arg: str | None) -> int | None:
     if arg == "a":
         return 2
+    return None
 
 
-def test_Registry_get():
+def test_Registry_get() -> None:
     r = Registry()
 
     r.register(factory_none)
@@ -57,20 +87,20 @@ def test_Registry_lifecycle():
 
     r.register(factory_none)
     assert len(r._entries) == 2
-    assert r._entries[0].factory is factory_all
-    assert r._entries[1].factory is factory_none
+    assert r.at(0).factory is factory_all
+    assert r.at(1).factory is factory_none
 
     r.register(factory_none, r.PRIORITY_FIRST)
     assert len(r._entries) == 3
-    assert r._entries[0].factory is factory_none
-    assert r._entries[0].priority == r.PRIORITY_FIRST
-    assert r._entries[1].factory is factory_all
-    assert r._entries[2].factory is factory_none
-    assert r._entries[2].priority == r.PRIORITY_MIDDLE
+    assert r.at(0).factory is factory_none
+    assert r.at(0).priority == r.PRIORITY_FIRST
+    assert r.at(1).factory is factory_all
+    assert r.at(2).factory is factory_none
+    assert r.at(2).priority == r.PRIORITY_MIDDLE
 
     r.unregister(factory_none)
     assert len(r._entries) == 1
-    assert r._entries[0].factory is factory_all
+    assert r.at(0).factory is factory_all
 
     r.unregister(factory_all)
     assert len(r._entries) == 0
