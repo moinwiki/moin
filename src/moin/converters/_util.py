moinwiki/moin

commit 07eb655b7790065a60b183f9e8a3a33e77a8ff95

Converters: avoid accessing flask globals in converter code

src/moin/converters/_util.py
@@ -9,13 +9,8 @@ MoinMoin - converter utilities.
 from __future__ import annotations
 
 from typing import Any
-from emeraldtree import ElementTree as ET
 
-try:
-    from moin import flaskg
-except ImportError:
-    # in case converters become an independent package
-    flaskg = None
+from emeraldtree import ElementTree as ET
 
 from moin.constants.misc import URI_SCHEMES
 from moin.log import getLogger
@@ -115,6 +110,7 @@ class _Iter:
 
 
 class _Stack:
+
     class Item:
         def __init__(self, elem):
             self.elem = elem
@@ -123,12 +119,13 @@ class _Stack:
             else:
                 self.name = None
 
-    def __init__(self, bottom=None, iter_content=None):
+    def __init__(self, bottom=None, iter_content=None, add_lineno: bool = False) -> None:
         self._list = []
         if bottom:
             self._list.append(self.Item(bottom))
         self.iter_content = iter_content
-        self.last_lineno = 0
+        self.last_lineno_attr = 0
+        self._add_lineno = add_lineno
 
     def __len__(self):
         return len(self._list)
@@ -137,11 +134,10 @@ class _Stack:
         """
         Add a custom attribute (data-lineno=nn) that will be used by Javascript to scroll edit textarea.
         """
-        if flaskg and getattr(flaskg, "add_lineno_attr", False):
-            if self.last_lineno != self.iter_content.lineno:
-                # avoid adding same lineno to parent and multiple children or grand-children
-                elem.attrib[html.data_lineno] = self.iter_content.lineno
-                self.last_lineno = self.iter_content.lineno
+        if self._add_lineno and self.last_lineno != self.iter_content.lineno:
+            # avoid adding same lineno to parent and multiple children or grand-children
+            elem.attrib[html.data_lineno] = self.iter_content.lineno
+            self.last_lineno = self.iter_content.lineno
 
     def clear(self):
         del self._list[1:]

src/moin/converters/base.py
@@ -3,18 +3,25 @@
 
 from __future__ import annotations
 
+from typing import Any, cast
+
 import logging
 
 from moin.log import getLogger
 from moin.converters import ConverterMessage
+from moin.converters._util import _Stack
 
 logger = getLogger(__name__)
 
 
 class ConverterBase:
 
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, **kwargs: Any) -> None:
         self.messages: set[ConverterMessage] = set()
+        self.add_lineno = cast(bool, kwargs.get("add_lineno", False))
+
+    def make_stack(self, bottom=None, iter_content=None) -> _Stack:
+        return _Stack(bottom, iter_content, self.add_lineno)
 
     def log(self, message: str, category: str) -> None:
         self.messages.add(ConverterMessage(message, category))

src/moin/converters/creole_in.py
@@ -37,7 +37,7 @@ from moin.utils.mime import Type, type_moin_document, type_moin_creole
 from . import default_registry
 from ._args_wiki import parse as parse_arguments
 from ._wiki_macro import ConverterMacro
-from ._util import decode_data, normalize_split_text, _Iter, _Stack
+from ._util import decode_data, normalize_split_text, _Iter
 
 if TYPE_CHECKING:
     from moin.converters._args import Arguments
@@ -637,7 +637,7 @@ class Converter(ConverterMacro):
 
         body = moin_page.body(attrib=attrib)
 
-        stack = _Stack(body, iter_content=iter_content)
+        stack = self.make_stack(body, iter_content=iter_content)
 
         # Please note that the iterator can be modified by other functions
         for line in iter_content:
@@ -666,7 +666,7 @@ class Converter(ConverterMacro):
         """
         p = moin_page.p()
         iter_content = _Iter(text)
-        stack = _Stack(p, iter_content=iter_content)
+        stack = self.make_stack(p, iter_content=iter_content)
         self.parse_inline(text, stack, self.inline_re)
         return p
 

src/moin/converters/docbook_in.py
@@ -22,12 +22,6 @@ import re
 
 from emeraldtree import ElementTree as ET
 
-try:
-    from moin import flaskg
-except ImportError:
-    # in case converters become an independent package
-    flaskg = None
-
 from moin.converters.base import ConverterBase
 from moin.log import getLogger
 from moin.utils.iri import Iri
@@ -49,12 +43,12 @@ class NameSpaceError(Exception):
     pass
 
 
-def XML(text, parser=None):
+def XML(text, add_lineno: bool, parser: XMLParser | None = None) -> Element:
     """
     Copied from EmeraldTree/tree.py to force use of local XMLParser class override.
     """
     if not parser:
-        parser = XMLParser(target=ET.TreeBuilder())
+        parser = XMLParser(target=ET.TreeBuilder(), add_lineno=add_lineno)
     parser.feed(text)
     return parser.close()
 
@@ -65,12 +59,16 @@ class XMLParser(ET.XMLParser):
 
     There is no need to subclass all tree.py classes and procedures with stubs because this
     modified _start_list is only needed during the initial construction of the DOM when
-    flaskg.add_lineno_attr may be True.
+    `add_lineno` may be True.
     """
+
+    def __init__(self, html=0, target=None, encoding=None, add_lineno=False):
+        super().__init__(html, target, encoding)
+        self.add_lineno = add_lineno
 
     def _start_list(self, tag, attrib_in):
         elem = super()._start_list(tag, attrib_in)
-        if flaskg and getattr(flaskg, "add_lineno_attr", False):
+        if self.add_lineno:
             elem.attrib[html.data_lineno] = self._parser.CurrentLineNumber
         return elem
 
@@ -456,7 +454,7 @@ class Converter(ConverterBase):
         # We will create an element tree from the DocBook content
         try:
             # using local XML override, not ET.XML
-            tree = XML(content)
+            tree = XML(content, self.add_lineno)
         except ET.ParseError as detail:
             return self.error(str(detail))
 

src/moin/converters/markdown_in.py
@@ -30,12 +30,6 @@ from ._util import decode_data, sanitise_uri_scheme
 
 from emeraldtree import ElementTree as ET
 
-try:
-    from moin import flaskg
-except ImportError:
-    # in case converters become an independent package
-    flaskg = None
-
 from markdown import Markdown  # https://python-markdown.github.io/
 import markdown.util as md_util
 from markdown.extensions.extra import ExtraExtension
@@ -263,7 +257,7 @@ class Converter(ConverterBase, html_in.HtmlTags):
 
         return text
 
-    def do_children(self, element, add_lineno=False) -> list[ET.Element | str]:
+    def do_children(self, element) -> list[ET.Element | str]:
         """
         Convert the children of `element` to EmeraldTree nodes.
 
@@ -287,7 +281,7 @@ class Converter(ConverterBase, html_in.HtmlTags):
             if r is None:
                 r = ()
             elif not isinstance(r, (list, tuple)):
-                if add_lineno and self.line_numbers:
+                if self.add_lineno and self.line_numbers:
                     # the line numbers for the start of each block were counted and saved before preprocessors were run
                     r.attrib[html.data_lineno] = self.line_numbers.popleft()
                 r = (r,)
@@ -458,7 +452,7 @@ class Converter(ConverterBase, html_in.HtmlTags):
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
 
-        self.html_in_converter = html_in.Converter()
+        self.html_in_converter = html_in.Converter(**kwargs)
 
         # share messages with the HTML-In converter
         self.html_in_converter.messages = self.messages
@@ -535,10 +529,8 @@ class Converter(ConverterBase, html_in.HtmlTags):
 
         # }}} end Markdown 3.0.0 core.py convert method
 
-        # run markdown post processors and convert from ElementTree
-        # to a list of EmeraldTree nodes:
-        add_lineno = bool(flaskg and getattr(flaskg, "add_lineno_attr", False))
-        page_children = self.do_children(root, add_lineno=add_lineno)
+        # run markdown post processors and convert from ElementTree to a list of EmeraldTree nodes
+        page_children = self.do_children(root)
 
         # convert HTML markup in text strings to EmeraldTree elements
         self.convert_html_markup(page_children)

src/moin/converters/mediawiki_in.py
@@ -35,7 +35,7 @@ from moin.utils.mime import Type, type_moin_document
 from . import default_registry
 from ._args import Arguments
 from ._wiki_macro import ConverterMacro
-from ._util import decode_data, normalize_split_text, _Iter, _Stack
+from ._util import decode_data, normalize_split_text, _Iter
 
 if TYPE_CHECKING:
     from moin.converters._args import Arguments
@@ -343,7 +343,7 @@ class Converter(ConverterMacro):
             if list_definition:
                 element_label = moin_page.list_item_label()
                 stack.top_append(element_label)
-                new_stack = _Stack(element_label, iter_content=iter_content)
+                new_stack = self.make_stack(element_label, iter_content=iter_content)
                 # TODO: definition list doesn't work,
                 #       if definition of the term on the next line
                 splited_text = text.split(":")
@@ -356,7 +356,7 @@ class Converter(ConverterMacro):
             element_body.level, element_body.type = level, type
 
             stack.push(element_body)
-            new_stack = _Stack(element_body, iter_content=iter_content)
+            new_stack = self.make_stack(element_body, iter_content=iter_content)
         else:
             new_stack = stack
             level = 0
@@ -1008,7 +1008,7 @@ class Converter(ConverterMacro):
 
         body = moin_page.body(attrib=attrib)
 
-        stack = _Stack(body, iter_content=iter_content)
+        stack = self.make_stack(body, iter_content=iter_content)
 
         for line in iter_content:
             match = self.indent_re.match(line)

src/moin/converters/moinwiki_in.py
@@ -20,17 +20,17 @@ from urllib.parse import urlencode, unquote
 
 from moin.constants.contenttypes import CHARSET
 from moin.constants.misc import URI_SCHEMES
+from moin.i18n import _
 from moin.log import getLogger
 from moin.utils.iri import Iri
 from moin.utils.tree import moin_page, xlink, xinclude, html
 from moin.utils.interwiki import is_known_wiki
 from moin.utils.mime import Type, type_moin_document, type_moin_wiki
-from moin.i18n import _
 
 from ._args import Arguments
 from ._args_wiki import parse as parse_arguments, object_re
 from ._wiki_macro import ConverterMacro
-from ._util import decode_data, normalize_split_text, _Iter, _Stack
+from ._util import decode_data, normalize_split_text, _Iter
 from . import default_registry
 
 if TYPE_CHECKING:
@@ -502,7 +502,7 @@ class Converter(ConverterMacro):
             if list_definition_text:
                 element_label = moin_page.list_item_label()
                 stack.top_append(element_label)
-                new_stack = _Stack(element_label, iter_content=iter_content)
+                new_stack = self.make_stack(element_label, iter_content=iter_content)
 
                 self.parse_inline(list_definition_text, new_stack, self.inline_re)
             if not list_definition_text or text:
@@ -511,7 +511,7 @@ class Converter(ConverterMacro):
                 element_body.level, element_body.type = level, type
 
                 stack.push(element_body)
-                new_stack = _Stack(element_body, iter_content=iter_content)
+                new_stack = self.make_stack(element_body, iter_content=iter_content)
         else:
             new_stack = stack
 
@@ -1090,7 +1090,7 @@ class Converter(ConverterMacro):
                     attrib[moin_page(key)] = value
 
         body = moin_page.body(attrib=attrib)
-        stack = _Stack(body, iter_content=iter_content)
+        stack = self.make_stack(body, iter_content=iter_content)
 
         for line in iter_content:
             data = {str(k): v for k, v in self.indent_re.match(line).groupdict().items() if v is not None}
@@ -1124,7 +1124,7 @@ class Converter(ConverterMacro):
         """
         p = moin_page.p()
         iter_content = _Iter(text)
-        stack = _Stack(p, iter_content=iter_content)
+        stack = self.make_stack(p, iter_content=iter_content)
         self.parse_inline(text, stack, self.inline_re)
         return p
 

src/moin/converters/rst_in.py
@@ -32,12 +32,6 @@ from docutils import core, nodes, transforms, utils
 from docutils.parsers.rst import Directive, directives
 import docutils.parsers.rst.directives.misc
 
-try:
-    from moin import flaskg
-except ImportError:
-    # in case converters become an independent package
-    flaskg = None
-
 from moin.converters.base import ConverterBase
 from moin.log import getLogger
 from moin.utils.iri import Iri
@@ -63,11 +57,12 @@ class NodeVisitor:
     https://docutils.sourceforge.io/docs/ref/doctree.html
     """
 
-    def __init__(self):
+    def __init__(self, add_lineno: bool) -> None:
         self.current_node = moin_page.body()
         self.root = moin_page.page(children=(self.current_node,))
         self.path = [self.root, self.current_node]
         self.header_size = 1
+        self.add_lineno = add_lineno
         self.last_lineno = 0
         self.current_lineno = 0
         self.footnote_ids = set()  # IDs of already handled footnotes
@@ -116,7 +111,7 @@ class NodeVisitor:
     # -----------------
 
     def open_moin_page_node(self, mointree_element, node=None):
-        if flaskg and getattr(flaskg, "add_lineno_attr", False):
+        if self.add_lineno:
             # add data-lineno attribute for auto-scrolling edit textarea
             if self.last_lineno < self.current_lineno:
                 mointree_element.attrib[html.data_lineno] = self.current_lineno
@@ -1192,7 +1187,7 @@ class Converter(ConverterBase):
                     input = [".. error::\n ::\n\n  {}\n\n".format(str(inst).replace("\n", "\n  "))]
                 raise inst
             break
-        visitor = NodeVisitor()
+        visitor = NodeVisitor(self.add_lineno)
         walkabout(docutils_tree, visitor)
         ret = visitor.tree()
         return ret

commit f61fd9b81625fabe9cba2894910b1dc56305a668

Fix issue: class `_Stack` has no member `last_lineno`
This fixes an issue introduced in commit 07eb655 (part of pull request #2306).

src/moin/converters/_util.py
@@ -124,7 +124,7 @@ class _Stack:
         if bottom:
             self._list.append(self.Item(bottom))
         self.iter_content = iter_content
-        self.last_lineno_attr = 0
+        self.last_lineno = 0
         self._add_lineno = add_lineno
 
     def __len__(self):